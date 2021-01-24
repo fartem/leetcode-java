262a107ccd3d8bf2ca5f4403bc4d46f992863f3c
fartem <jaman.smlnsk@gmail.com>
2021-01-24 08:19:11 +0300

    2021-01-24 Update: Added "Reverse Linked List"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/ReverseLinkedList.java
@@ -0,0 +1,27 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.ListNode;
+
+// https://leetcode.com/problems/reverse-linked-list
+public class ReverseLinkedList {
+
+    private final ListNode head;
+
+    public ReverseLinkedList(ListNode head) {
+        this.head = head;
+    }
+
+    public ListNode solution() {
+        ListNode previous = null;
+        ListNode current = head;
+        ListNode next;
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/ReverseLinkedListTest.java
@@ -0,0 +1,50 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.ListNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReverseLinkedListTest {
+
+    @Test
+    public void defaultTest() {
+        ListNode validList = new ListNode(
+                5,
+                new ListNode(
+                        4,
+                        new ListNode(
+                                3,
+                                new ListNode(
+                                        2,
+                                        new ListNode(1)
+                                )
+                        )
+                )
+        );
+        ListNode reversedList = new ReverseLinkedList(
+                new ListNode(
+                        1,
+                        new ListNode(
+                                2,
+                                new ListNode(
+                                        3,
+                                        new ListNode(
+                                                4,
+                                                new ListNode(5)
+                                        )
+                                )
+                        )
+                )
+        ).solution();
+        while (validList != null) {
+            assertEquals(
+                    validList.val,
+                    reversedList.val
+            );
+            validList = validList.next;
+            reversedList = reversedList.next;
+        }
+    }
+
+}
