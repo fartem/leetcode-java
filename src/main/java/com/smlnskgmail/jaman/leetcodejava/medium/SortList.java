9ce0bce47133123e63ef2048b1bb0b9ec5bc0fa9
fartem <jaman.smlnsk@gmail.com>
2021-01-27 09:45:49 +0300

    2021-01-27 Update: Added "Sort List"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/SortList.java
@@ -0,0 +1,64 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.ListNode;
+
+// https://leetcode.com/problems/sort-list
+public class SortList {
+
+    private final ListNode input;
+
+    public SortList(ListNode input) {
+        this.input = input;
+    }
+
+    public ListNode solution() {
+        return sort(input);
+    }
+
+    private ListNode sort(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode temp = head;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            temp = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        temp.next = null;
+
+        ListNode left = sort(head);
+        ListNode right = sort(slow);
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode temp = new ListNode(0);
+        ListNode current = temp;
+
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                current.next = left;
+                left = left.next;
+            } else {
+                current.next = right;
+                right = right.next;
+            }
+            current = current.next;
+        }
+
+        if (left != null) {
+            current.next = left;
+        }
+        if (right != null) {
+            current.next = right;
+        }
+        return temp.next;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/SortListTest.java
@@ -0,0 +1,45 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.ListNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SortListTest {
+
+    @Test
+    public void defaultTest() {
+        // 4213
+        ListNode validList = new ListNode(
+                1,
+                new ListNode(
+                        2,
+                        new ListNode(
+                                3,
+                                new ListNode(4)
+                        )
+                )
+        );
+        ListNode sortedList = new SortList(
+                new ListNode(
+                        4,
+                        new ListNode(
+                                2,
+                                new ListNode(
+                                        1,
+                                        new ListNode(3)
+                                )
+                        )
+                )
+        ).solution();
+        while (validList != null) {
+            assertEquals(
+                    validList.val,
+                    sortedList.val
+            );
+            validList = validList.next;
+            sortedList = sortedList.next;
+        }
+    }
+
+}
