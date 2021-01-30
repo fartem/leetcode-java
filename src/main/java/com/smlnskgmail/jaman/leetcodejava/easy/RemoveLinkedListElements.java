c53d2287027090d106d2168911631e83f70b291b
fartem <jaman.smlnsk@gmail.com>
2021-01-30 09:15:38 +0300

    2021-01-30 Update: Added "Remove Linked List Elements"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/RemoveLinkedListElements.java
@@ -0,0 +1,31 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.ListNode;
+
+// https://leetcode.com/problems/remove-linked-list-elements
+public class RemoveLinkedListElements {
+
+    private ListNode head;
+    private final int target;
+
+    public RemoveLinkedListElements(ListNode head, int target) {
+        this.head = head;
+        this.target = target;
+    }
+
+    public ListNode solution() {
+        while (head != null && head.val == target) {
+            head = head.next;
+        }
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.next.val == target) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/RemoveLinkedListElementsTest.java
@@ -0,0 +1,57 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.ListNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RemoveLinkedListElementsTest {
+
+    @Test
+    public void defaultTest() {
+        ListNode validList = new ListNode(
+                1,
+                new ListNode(
+                        2,
+                        new ListNode(
+                                3,
+                                new ListNode(
+                                        4,
+                                        new ListNode(5)
+                                )
+                        )
+                )
+        );
+        ListNode result = new RemoveLinkedListElements(
+                new ListNode(
+                        1,
+                        new ListNode(
+                                2,
+                                new ListNode(
+                                        6,
+                                        new ListNode(
+                                                3,
+                                                new ListNode(
+                                                        4,
+                                                        new ListNode(
+                                                                5,
+                                                                new ListNode(6)
+                                                        )
+                                                )
+                                        )
+                                )
+                        )
+                ),
+                6
+        ).solution();
+        while (validList != null) {
+            assertEquals(
+                    validList.val,
+                    result.val
+            );
+            validList = validList.next;
+            result = result.next;
+        }
+    }
+
+}
