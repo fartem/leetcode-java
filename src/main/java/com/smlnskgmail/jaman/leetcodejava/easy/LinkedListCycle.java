1ed26b3216062947578121739c11a77d9cec940b
fartem <jaman.smlnsk@gmail.com>
2021-02-03 08:05:19 +0300

    2021-02-03 Update: Added "Linked List Cycle"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/LinkedListCycle.java
@@ -0,0 +1,30 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.ListNode;
+
+// https://leetcode.com/problems/linked-list-cycle
+public class LinkedListCycle {
+
+    private final ListNode input;
+
+    public LinkedListCycle(ListNode input) {
+        this.input = input;
+    }
+
+    public boolean solution() {
+        if (input == null) {
+            return false;
+        }
+        ListNode slow = input;
+        ListNode fast = input.next;
+        while (slow != fast) {
+            if (fast == null && fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/LinkedListCycleTest.java
@@ -0,0 +1,28 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.ListNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class LinkedListCycleTest {
+
+    @Test
+    public void defaultTest() {
+        ListNode[] nodes = new ListNode[4];
+        nodes[0] = new ListNode(3);
+        nodes[1] = new ListNode(2);
+        nodes[0].next = nodes[1];
+        nodes[2] = new ListNode(0);
+        nodes[1].next = nodes[2];
+        nodes[3] = new ListNode(-4);
+        nodes[2].next = nodes[3];
+        nodes[3].next = nodes[1];
+        assertTrue(
+                new LinkedListCycle(
+                        nodes[0]
+                ).solution()
+        );
+    }
+
+}
