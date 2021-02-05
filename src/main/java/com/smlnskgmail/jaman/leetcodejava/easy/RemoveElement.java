8b3ec07a884b481357b6cd7490a41e592630f4de
fartem <jaman.smlnsk@gmail.com>
2021-02-05 14:15:09 +0300

    2021-02-05 Update: Added "Remove Element"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/RemoveElement.java
@@ -0,0 +1,25 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/remove-element
+public class RemoveElement {
+
+    private final int[] input;
+    private final int value;
+
+    public RemoveElement(int[] input, int value) {
+        this.input = input;
+        this.value = value;
+    }
+
+    public int solution() {
+        int index = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] != value) {
+                input[index] = input[i];
+                index++;
+            }
+        }
+        return index;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/RemoveElementTest.java
@@ -0,0 +1,25 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RemoveElementTest {
+
+    @Test
+    public void defaultTest() {
+        final int[] input = new int[]{3, 2, 2, 3};
+        final int value = 3;
+        final int index = new RemoveElement(
+                input,
+                value
+        ).solution();
+        for (int i = 0; i < index; i++) {
+            assertEquals(
+                    2,
+                    input[i]
+            );
+        }
+    }
+
+}
