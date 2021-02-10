1f9a2ff31ead5149027b771923f68d9dcf3e3774
fartem <jaman.smlnsk@gmail.com>
2021-02-10 08:58:57 +0300

    2021-02-10 Update: Added "Find the Difference"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/FindTheDifference.java
@@ -0,0 +1,34 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/find-the-difference
+public class FindTheDifference {
+
+    private final String firstString;
+    private final String secondString;
+
+    public FindTheDifference(String firstString, String secondString) {
+        this.firstString = firstString;
+        this.secondString = secondString;
+    }
+
+    public char solution() {
+        if (firstString.length() == 0) {
+            return secondString.charAt(0);
+        }
+        char[] sChars = firstString.toCharArray();
+        char[] tChars = secondString.toCharArray();
+
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        for (int i = 0; i < sChars.length; i++) {
+            if (sChars[i] != tChars[i]) {
+                return tChars[i];
+            }
+        }
+        return tChars[tChars.length - 1];
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/FindTheDifferenceTest.java
@@ -0,0 +1,20 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FindTheDifferenceTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                'e',
+                new FindTheDifference(
+                        "abcd",
+                        "abcde"
+                ).solution()
+        );
+    }
+
+}
