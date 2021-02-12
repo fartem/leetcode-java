384a3437580fd657e0c6176836bf370e3edde843
fartem <jaman.smlnsk@gmail.com>
2021-02-12 08:36:24 +0300

    2021-02-12 Update: Added "Intersection of Two Linked Lists"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,34 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.ListNode;
+
+//https://leetcode.com/problems/intersection-of-two-linked-lists
+public class IntersectionOfTwoLinkedLists {
+
+    private ListNode firstHead;
+    private ListNode secondHead;
+
+    public IntersectionOfTwoLinkedLists(ListNode firstHead, ListNode secondHead) {
+        this.firstHead = firstHead;
+        this.secondHead = secondHead;
+    }
+
+    public ListNode solution() {
+        ListNode pointerA = firstHead;
+        ListNode pointerB = secondHead;
+        while (pointerA != pointerB) {
+            if (pointerA == null) {
+                pointerA = secondHead;
+            } else {
+                pointerA = pointerA.next;
+            }
+            if (pointerB == null) {
+                pointerB = firstHead;
+            } else {
+                pointerB = pointerB.next;
+            }
+        }
+        return pointerA;
+    }
+
+}
