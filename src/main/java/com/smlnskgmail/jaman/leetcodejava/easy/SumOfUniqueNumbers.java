c8c4c83526e8d3d00ba48794b2a212aa409bc092
fartem <jaman.smlnsk@gmail.com>
2021-02-17 08:34:20 +0300

    2021-02-17 Update: Added "Sum of Unique Numbers"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/SumOfUniqueNumbers.java
@@ -0,0 +1,33 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode.com/problems/sum-of-unique-elements
+public class SumOfUniqueNumbers {
+
+    private final int[] input;
+
+    public SumOfUniqueNumbers(int[] input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        Map<Integer, Integer> numbers = new HashMap<>();
+        int result = 0;
+        for (int num : input) {
+            if (numbers.containsKey(num)) {
+                int count = numbers.get(num);
+                if (count == 1) {
+                    result -= num;
+                }
+                numbers.put(num, count + 1);
+            } else {
+                result += num;
+                numbers.put(num, 1);
+            }
+        }
+        return result;
+    }
+
+}
