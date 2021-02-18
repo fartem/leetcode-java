d08b98c3c72a201ca759ceed3a0a5bed23bf150d
fartem <jaman.smlnsk@gmail.com>
2021-02-18 08:23:10 +0300

    2021-02-18 Update: Added "Defanging an IP Address"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/DefangingAnIPAddress.java
@@ -0,0 +1,30 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/defanging-an-ip-address
+public class DefangingAnIPAddress {
+
+    private final String input;
+
+    public DefangingAnIPAddress(String input) {
+        this.input = input;
+    }
+
+    public String solution() {
+        char[] result = new char[input.length() + 6];
+        int resultPointer = 0;
+        int addressPointer = 0;
+        while (addressPointer != input.length()) {
+            char c = input.charAt(addressPointer);
+            if (c == '.') {
+                result[resultPointer++] = '[';
+                result[resultPointer++] = '.';
+                result[resultPointer++] = ']';
+            } else {
+                result[resultPointer++] = c;
+            }
+            addressPointer++;
+        }
+        return String.valueOf(result);
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/DefangingAnIPAddressTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DefangingAnIPAddressTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                "1[.]1[.]1[.]1",
+                new DefangingAnIPAddress(
+                        "1.1.1.1"
+                ).solution()
+        );
+    }
+
+}
