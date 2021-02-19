c7d9cd8cf79a023d0c515d2a531c6f801bd630f1
fartem <jaman.smlnsk@gmail.com>
2021-02-19 07:01:34 +0300

    2021-02-19 Update: Added "Number of Steps to Reduce a Number to Zero"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,21 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+public class NumberOfStepsToReduceANumberToZero {
+
+    private int input;
+
+    public NumberOfStepsToReduceANumberToZero(int input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        int result = 0;
+        while (input != 0) {
+            input = input % 2 == 0 ? input / 2 : input - 1;
+            result++;
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/NumberOfStepsToReduceANumberToZeroTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NumberOfStepsToReduceANumberToZeroTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                6,
+                new NumberOfStepsToReduceANumberToZero(
+                        14
+                ).solution()
+        );
+    }
+
+}
