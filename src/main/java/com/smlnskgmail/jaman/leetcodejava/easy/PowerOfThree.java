0f5084009577399a5362e22b27fa8910edac2ae1
fartem <jaman.smlnsk@gmail.com>
2021-03-02 07:57:48 +0300

    2021-03-02 Update: Added "Power of Three"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/PowerOfThree.java
@@ -0,0 +1,22 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/power-of-three
+public class PowerOfThree {
+
+    private final int input;
+
+    public PowerOfThree(int input) {
+        this.input = input;
+    }
+
+    public boolean solution() {
+        int num = input / 3;
+        int pow = 0;
+        while (num != 0) {
+            num /= 3;
+            pow++;
+        }
+        return Math.pow(3, pow) == input;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/PowerOfThreeTest.java
@@ -0,0 +1,16 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PowerOfThreeTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(
+                new PowerOfThree(27).solution()
+        );
+    }
+
+}
