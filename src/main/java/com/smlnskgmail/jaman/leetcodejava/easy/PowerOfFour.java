c768ca47b76d5394bcd86fd3e282455e928a7338
fartem <jaman.smlnsk@gmail.com>
2021-03-03 08:01:17 +0300

    2021-03-03 Update: Added "Power of Four"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/PowerOfFour.java
@@ -0,0 +1,22 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/power-of-four
+public class PowerOfFour {
+
+    private final int input;
+
+    public PowerOfFour(int input) {
+        this.input = input;
+    }
+
+    public boolean solution() {
+        int num = input / 4;
+        int pow = 0;
+        while (num != 0) {
+            num /= 4;
+            pow++;
+        }
+        return Math.pow(4, pow) == input;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/PowerOfFourTest.java
@@ -0,0 +1,16 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PowerOfFourTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(
+                new PowerOfFour(16).solution()
+        );
+    }
+
+}
