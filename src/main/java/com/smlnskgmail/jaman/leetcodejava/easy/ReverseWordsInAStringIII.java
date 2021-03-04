337d3627bfb0794a2aa3d18aa4c31729f5ba4ead
fartem <jaman.smlnsk@gmail.com>
2021-03-04 08:20:35 +0300

    2021-03-04 Update: Added "Reverse Words in a String III"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/ReverseWordsInAStringIII.java
@@ -0,0 +1,20 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/reverse-words-in-a-string-iii
+public class ReverseWordsInAStringIII {
+
+    private final String input;
+
+    public ReverseWordsInAStringIII(String input) {
+        this.input = input;
+    }
+
+    public String solution() {
+        StringBuilder result = new StringBuilder();
+        for (String word : input.split(" ")) {
+            result.append(" ").append(new StringBuilder(word).reverse());
+        }
+        return result.toString().trim();
+    }
+
+}
