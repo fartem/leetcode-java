11ac83d0bc5e8cdafc479cccaf41993d2a4848f3
fartem <jaman.smlnsk@gmail.com>
2021-03-06 09:30:29 +0300

    2021-03-06 Update: Added "Design Browser History"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/DesignBrowserHistory.java
@@ -0,0 +1,45 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/design-browser-history
+public class DesignBrowserHistory {
+
+    private final List<String> history = new ArrayList<>();
+    private int current = 0;
+
+    public DesignBrowserHistory(String homepage) {
+        history.add(homepage);
+    }
+
+    public void visit(String url) {
+        if (current < history.size() - 1) {
+            int length = history.size() - 1;
+            for (int i = current; i < length; i++) {
+                history.remove(history.size() - 1);
+            }
+        }
+        history.add(url);
+        current++;
+    }
+
+    public String back(int steps) {
+        if (current < steps) {
+            current = 0;
+        } else {
+            current = current - steps;
+        }
+        return history.get(current);
+    }
+
+    public String forward(int steps) {
+        if (current + steps >= history.size()) {
+            current = history.size() - 1;
+        } else {
+            current = current + steps;
+        }
+        return history.get(current);
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/DesignBrowserHistoryTest.java
@@ -0,0 +1,42 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DesignBrowserHistoryTest {
+
+    @Test
+    public void defaultTest() {
+        DesignBrowserHistory designBrowserHistory = new DesignBrowserHistory("leetcode.com");
+        designBrowserHistory.visit("google.com");
+        designBrowserHistory.visit("facebook.com");
+        designBrowserHistory.visit("youtube.com");
+        assertEquals(
+                "facebook.com",
+                designBrowserHistory.back(1)
+        );
+        assertEquals(
+                "google.com",
+                designBrowserHistory.back(1)
+        );
+        assertEquals(
+                "facebook.com",
+                designBrowserHistory.forward(1)
+        );
+        designBrowserHistory.visit("linkedin.com");     // You are in "facebook.com". Visit "linkedin.com"
+        assertEquals(
+                "linkedin.com",
+                designBrowserHistory.forward(2)
+        );
+        assertEquals(
+                "google.com",
+                designBrowserHistory.back(2)
+        );
+        assertEquals(
+                "leetcode.com",
+                designBrowserHistory.back(7)
+        );
+    }
+
+}
