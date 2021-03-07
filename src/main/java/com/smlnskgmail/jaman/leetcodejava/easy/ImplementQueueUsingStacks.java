7f2e0e5cca340ca08eac77700a25aa25ef258cc0
fartem <jaman.smlnsk@gmail.com>
2021-03-07 11:24:55 +0300

    2021-03-07 Update: Added "Implement Queue using Stacks"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/ImplementQueueUsingStacks.java
@@ -0,0 +1,34 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.Stack;
+
+// https://leetcode.com/problems/implement-queue-using-stacks
+public class ImplementQueueUsingStacks {
+
+    private final Stack<Integer> stack = new Stack<>();
+
+    public void push(int x) {
+        stack.push(x);
+    }
+
+    public int pop() {
+        Stack<Integer> tempStack = new Stack<>();
+        while (!stack.isEmpty()) {
+            tempStack.push(stack.pop());
+        }
+        int element = tempStack.pop();
+        while (!tempStack.isEmpty()) {
+            stack.push(tempStack.pop());
+        }
+        return element;
+    }
+
+    public int peek() {
+        return stack.get(0);
+    }
+
+    public boolean empty() {
+        return stack.isEmpty();
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/ImplementQueueUsingStacksTest.java
@@ -0,0 +1,28 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class ImplementQueueUsingStacksTest {
+
+    @Test
+    public void defaultTest() {
+        ImplementQueueUsingStacks implementQueueUsingStacks = new ImplementQueueUsingStacks();
+        implementQueueUsingStacks.push(1);
+        implementQueueUsingStacks.push(2);
+        assertEquals(
+                1,
+                implementQueueUsingStacks.peek()
+        );
+        assertEquals(
+                1,
+                implementQueueUsingStacks.pop()
+        );
+        assertFalse(
+                implementQueueUsingStacks.empty()
+        );
+    }
+
+}
