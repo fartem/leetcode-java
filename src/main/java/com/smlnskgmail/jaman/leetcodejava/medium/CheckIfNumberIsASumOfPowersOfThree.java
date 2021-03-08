4f5115de8816a70ffff10945c4e2b1811e305a71
fartem <jaman.smlnsk@gmail.com>
2021-03-08 20:34:14 +0300

    2021-03-08 Update: Added "Check if Number is a Sum of Powers of Three"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/CheckIfNumberIsASumOfPowersOfThree.java
@@ -0,0 +1,16 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+// https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three
+public class CheckIfNumberIsASumOfPowersOfThree {
+
+    private final int input;
+
+    public CheckIfNumberIsASumOfPowersOfThree(int input) {
+        this.input = input;
+    }
+
+    public boolean solution() {
+        return Integer.toString(input, 3).indexOf('2') == -1;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/CheckIfNumberIsASumOfPowersOfThreeTest.java
@@ -0,0 +1,16 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CheckIfNumberIsASumOfPowersOfThreeTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(
+                new CheckIfNumberIsASumOfPowersOfThree(12).solution()
+        );
+    }
+
+}
