5dd496b97289cd5f42d19c491361d092af966c47
fartem <jaman.smlnsk@gmail.com>
2021-03-13 09:37:35 +0300

    2021-03-13 Update: Added "Design Parking System"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/DesignParkingSystem.java
@@ -0,0 +1,39 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/design-parking-system
+public class DesignParkingSystem {
+
+    private int big;
+    private int medium;
+    private int small;
+
+    public DesignParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public boolean addCar(int carType) {
+        switch (carType) {
+            case 1:
+                if (big - 1 >= 0) {
+                    big--;
+                    return true;
+                }
+                return false;
+            case 2:
+                if (medium - 1 >= 0) {
+                    medium--;
+                    return true;
+                }
+                return false;
+            default:
+                if (small - 1 >= 0) {
+                    small--;
+                    return true;
+                }
+                return false;
+        }
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/DesignParkingSystemTest.java
@@ -0,0 +1,26 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DesignParkingSystemTest {
+
+    @Test
+    public void defaultTest() {
+        DesignParkingSystem designParkingSystem = new DesignParkingSystem(1, 1, 0);
+        assertTrue(
+                designParkingSystem.addCar(1)
+        );
+        assertTrue(
+                designParkingSystem.addCar(2)
+        );
+        assertFalse(
+                designParkingSystem.addCar(3)
+        );
+        assertFalse(
+                designParkingSystem.addCar(1)
+        );
+    }
+
+}
