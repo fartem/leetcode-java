7cf340b22949878d51608e4c7068d9ff3fe07c98
fartem <jaman.smlnsk@gmail.com>
2021-03-22 07:49:25 +0300

    2021-03-22 Update: Added "Reverse String"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/ReverseString.java
@@ -0,0 +1,21 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/reverse-string
+public class ReverseString {
+
+    private final char[] input;
+
+    public ReverseString(char[] input) {
+        this.input = input;
+    }
+
+    public void solution() {
+        int max = input.length - 1;
+        for (int i = 0; i < input.length / 2; i++) {
+            char temp = input[i];
+            input[i] = input[max - i];
+            input[max - i] = temp;
+        }
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/ReverseStringTest.java
@@ -0,0 +1,20 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class ReverseStringTest {
+
+    @Test
+    public void defaultTest() {
+        char[] input = new char[]{'h', 'e', 'l', 'l', 'o'};
+        ReverseString reverseString = new ReverseString(input);
+        reverseString.solution();
+        assertArrayEquals(
+                new char[]{'o', 'l', 'l', 'e', 'h'},
+                input
+        );
+    }
+
+}
