3c028b999b6301e8e29921b52664e2ccff8fe894
fartem <jaman.smlnsk@gmail.com>
2021-03-30 08:42:08 +0300

    2021-03-30 Update: Added "XOR Operation in an Array"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/XOROperationInAnArray.java
@@ -0,0 +1,24 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/xor-operation-in-an-array
+public class XOROperationInAnArray {
+
+    private final int number;
+    private final int start;
+
+    public XOROperationInAnArray(int number, int start) {
+        this.number = number;
+        this.start = start;
+    }
+
+    public int solution() {
+        int result = 0;
+        int i = 0;
+        while (i != number) {
+            result ^= start + 2 * i;
+            i++;
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/XOROperationInAnArrayTest.java
@@ -0,0 +1,20 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class XOROperationInAnArrayTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                8,
+                new XOROperationInAnArray(
+                        5,
+                        0
+                ).solution()
+        );
+    }
+
+}
