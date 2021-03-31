e72ab448b9af8d96f2ea9c88c29d528d97748128
fartem <jaman.smlnsk@gmail.com>
2021-03-31 08:02:58 +0300

    2021-03-31 Update: Added "Kids With the Greatest Number of Candies"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,31 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies
+public class KidsWithTheGreatestNumberOfCandies {
+
+    private final int[] candies;
+    private final int extraCandies;
+
+    public KidsWithTheGreatestNumberOfCandies(int[] candies, int extraCandies) {
+        this.candies = candies;
+        this.extraCandies = extraCandies;
+    }
+
+    public List<Boolean> solution() {
+        List<Boolean> result = new ArrayList<>(candies.length);
+        int max = Integer.MIN_VALUE;
+        for (int count : candies) {
+            if (count > max) {
+                max = count;
+            }
+        }
+        for (int count : candies) {
+            result.add(count + extraCandies >= max);
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/KidsWithTheGreatestNumberOfCandiesTest.java
@@ -0,0 +1,28 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class KidsWithTheGreatestNumberOfCandiesTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                Arrays.asList(
+                        true,
+                        true,
+                        true,
+                        false,
+                        true
+                ),
+                new KidsWithTheGreatestNumberOfCandies(
+                        new int[]{2, 3, 5, 1, 3},
+                        3
+                ).solution()
+        );
+    }
+
+}
