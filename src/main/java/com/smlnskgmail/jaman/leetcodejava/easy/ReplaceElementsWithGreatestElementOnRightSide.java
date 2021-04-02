54b33cd4f8eb80520ec917b655419caa496c7ca5
fartem <jaman.smlnsk@gmail.com>
2021-04-02 08:52:11 +0300

    2021-04-02 Update: Added "Replace Elements with Greatest Element on Right Side"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,35 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side
+public class ReplaceElementsWithGreatestElementOnRightSide {
+
+    private final int[] input;
+
+    public ReplaceElementsWithGreatestElementOnRightSide(int[] input) {
+        this.input = input;
+    }
+
+    public int[] solution() {
+        int length = input.length;
+        int index = 0;
+        int max = 0;
+        for (int i = 0; i < length - 1; i++) {
+            if (i < index) {
+                input[i] = max;
+            } else {
+                max = 0;
+                for (int j = i + 1; j < length; j++) {
+                    int num = input[j];
+                    if (num > max) {
+                        max = num;
+                        index = j;
+                    }
+                }
+                input[i] = max;
+            }
+        }
+        input[length - 1] = -1;
+        return input;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/ReplaceElementsWithGreatestElementOnRightSideTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReplaceElementsWithGreatestElementOnRightSideTest {
+
+    @Test
+    public void defaultTest() {
+        assertArrayEquals(
+                new int[]{18, 6, 6, 6, 1, -1},
+                new ReplaceElementsWithGreatestElementOnRightSide(
+                        new int[]{17, 18, 5, 4, 6, 1}
+                ).solution()
+        );
+    }
+
+}
