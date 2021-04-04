86ea3ecd77233dad16d8d70ea84a20df8f178a2c
fartem <jaman.smlnsk@gmail.com>
2021-04-04 19:53:25 +0300

    2021-04-04 Update: Added "Detect Capital"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/DetectCapital.java
@@ -0,0 +1,21 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/detect-capital
+public class DetectCapital {
+
+    private final String input;
+
+    public DetectCapital(String input) {
+        this.input = input;
+    }
+
+    public boolean solution() {
+        if (input.charAt(0) < 97) {
+            String sub = input.substring(1);
+            return sub.toLowerCase().equals(sub)
+                    || input.toUpperCase().equals(input);
+        }
+        return input.toLowerCase().equals(input);
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/DetectCapitalTest.java
@@ -0,0 +1,18 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DetectCapitalTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(
+                new DetectCapital(
+                        "USA"
+                ).solution()
+        );
+    }
+
+}
