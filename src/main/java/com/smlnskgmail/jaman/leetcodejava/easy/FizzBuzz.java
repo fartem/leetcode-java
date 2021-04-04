73ef5a8661effffd8923a09f9e2cff626d289544
fartem <jaman.smlnsk@gmail.com>
2021-04-04 10:03:05 +0300

    2021-04-04 Update: Added "Fizz Buzz"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/FizzBuzz.java
@@ -0,0 +1,31 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/fizz-buzz
+public class FizzBuzz {
+
+    private final int input;
+
+    public FizzBuzz(int input) {
+        this.input = input;
+    }
+
+    public List<String> solution() {
+        List<String> result = new ArrayList<>(input);
+        for (int i = 1; i <= input; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/FizzBuzzTest.java
@@ -0,0 +1,23 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                Collections.singletonList(
+                        "1"
+                ),
+                new FizzBuzz(
+                        1
+                ).solution()
+        );
+    }
+
+}
