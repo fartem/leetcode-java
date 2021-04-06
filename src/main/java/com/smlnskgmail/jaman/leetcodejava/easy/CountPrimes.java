a99c55c55a57af60f4e2a887c10d3806e8a9ac35
fartem <jaman.smlnsk@gmail.com>
2021-04-06 10:35:10 +0300

    2021-04-06 Update: Added "Count Primes"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/CountPrimes.java
@@ -0,0 +1,34 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/count-primes
+public class CountPrimes {
+
+    private final int input;
+
+    public CountPrimes(int input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        boolean[] isPrime = new boolean[input];
+        for (int i = 2; i < input; i++) {
+            isPrime[i] = true;
+        }
+        for (int i = 2; i * i < input; i++) {
+            if (!isPrime[i]) {
+                continue;
+            }
+            for (int j = i * i; j < input; j += i) {
+                isPrime[j] = false;
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < input; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/CountPrimesTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CountPrimesTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                4,
+                new CountPrimes(
+                        10
+                ).solution()
+        );
+    }
+
+}
