bb8ce02dd98862ecf12069957e5753fc54e3b921
fartem <jaman.smlnsk@gmail.com>
2021-04-08 07:55:46 +0300

    2021-04-08 Update: Added "Sqrt(x)"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/SqrtX.java
@@ -0,0 +1,16 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/sqrtx
+public class SqrtX {
+
+    private final int input;
+
+    public SqrtX(int input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        return (int) Math.sqrt(input);
+    }
+
+}
