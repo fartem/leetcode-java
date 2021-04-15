d8d88dd8e993d13e1035aa6cd2548a1541700b1b
fartem <jaman.smlnsk@gmail.com>
2021-04-15 21:55:25 +0300

    2021-04-15 Update: Added "Fibonacci Number"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/FibonacciNumber.java
@@ -0,0 +1,27 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/fibonacci-number
+public class FibonacciNumber {
+
+    private final int input;
+
+    public FibonacciNumber(int input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        if (input < 2) {
+            return input;
+        }
+        int first = 0;
+        int second = 1;
+        for (int i = 2; i < input; i += 2) {
+            first += second;
+            if (i < input - 1) {
+                second += first;
+            }
+        }
+        return first + second;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/FibonacciNumberTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FibonacciNumberTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                1,
+                new FibonacciNumber(
+                        2
+                ).solution()
+        );
+    }
+
+}
