9be36eb46d1cb4276d94a111a302652f453383dc
fartem <jaman.smlnsk@gmail.com>
2021-04-20 08:07:08 +0300

    2021-04-20 Update: Added "Convert a Number to Hexadecimal"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/ConvertANumberToHexadecimal.java
@@ -0,0 +1,16 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/convert-a-number-to-hexadecimal
+public class ConvertANumberToHexadecimal {
+
+    private final int input;
+
+    public ConvertANumberToHexadecimal(int input) {
+        this.input = input;
+    }
+
+    public String solution() {
+        return Integer.toHexString(input);
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/ConvertANumberToHexadecimalTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConvertANumberToHexadecimalTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                "1a",
+                new ConvertANumberToHexadecimal(
+                        26
+                ).solution()
+        );
+    }
+
+}
