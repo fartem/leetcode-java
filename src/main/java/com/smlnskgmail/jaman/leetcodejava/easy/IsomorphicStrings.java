c2e62fdcdefa4a41bc66ae2ea6cdba1cc937cbc5
fartem <jaman.smlnsk@gmail.com>
2021-04-25 09:44:11 +0300

    2021-04-25 Update: Added "Isomorphic Strings"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/IsomorphicStrings.java
@@ -0,0 +1,42 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// https://leetcode.com/problems/isomorphic-strings
+public class IsomorphicStrings {
+
+    private final String s;
+    private final String t;
+
+    public IsomorphicStrings(
+            String s,
+            String t
+    ) {
+        this.s = s;
+        this.t = t;
+    }
+
+    public boolean solution() {
+        char[] replaces = new char[128];
+        Set<Character> replaceValues = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            char currS = s.charAt(i);
+            char currT = t.charAt(i);
+            if (replaces[currS] != 0) {
+                char replacement = replaces[currS];
+                if (currT != replacement) {
+                    return false;
+                }
+            } else {
+                if (replaceValues.contains(currT)) {
+                    return false;
+                }
+                replaces[currS] = currT;
+                replaceValues.add(currT);
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/IsomorphicStringsTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class IsomorphicStringsTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(
+                new IsomorphicStrings(
+                        "egg",
+                        "add"
+                ).solution()
+        );
+    }
+
+}
