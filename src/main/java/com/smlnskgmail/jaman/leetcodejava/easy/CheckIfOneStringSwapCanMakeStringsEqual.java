94c7a3361cbf8535ad74c9c086bb249b1842ff4b
fartem <jaman.smlnsk@gmail.com>
2021-04-27 08:10:10 +0300

    2021-04-27 Update: Added "Check if One String Swap Can Make Strings Equal"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/CheckIfOneStringSwapCanMakeStringsEqual.java
@@ -0,0 +1,35 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal
+public class CheckIfOneStringSwapCanMakeStringsEqual {
+
+    private final String s1;
+    private final String s2;
+
+    public CheckIfOneStringSwapCanMakeStringsEqual(
+            String s1,
+            String s2
+    ) {
+        this.s1 = s1;
+        this.s2 = s2;
+    }
+
+    public boolean solution() {
+        char[] s1Chars = new char[128];
+        int diff = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            char s1Char = s1.charAt(i);
+            if (s1Char != s2.charAt(i)) {
+                diff++;
+            }
+            s1Chars[s1Char] = 1;
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            if (s1Chars[s2.charAt(i)] == 0) {
+                return false;
+            }
+        }
+        return diff == 0 || diff == 2;
+    }
+
+}
