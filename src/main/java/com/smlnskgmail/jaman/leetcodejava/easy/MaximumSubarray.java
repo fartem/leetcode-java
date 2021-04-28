9f1f301c52a3cbab7288d192735d514760e6d10e
fartem <jaman.smlnsk@gmail.com>
2021-04-28 08:04:15 +0300

    2021-04-28 Update: Added "Maximum Subarray"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/MaximumSubarray.java
@@ -0,0 +1,24 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/maximum-subarray
+public class MaximumSubarray {
+
+    private final int[] input;
+
+    public MaximumSubarray(int[] input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        int localMax = 0;
+        int globalMax = Integer.MIN_VALUE;
+        for (int num : input) {
+            localMax = Math.max(num, num + localMax);
+            if (globalMax < localMax) {
+                globalMax = localMax;
+            }
+        }
+        return globalMax;
+    }
+
+}
