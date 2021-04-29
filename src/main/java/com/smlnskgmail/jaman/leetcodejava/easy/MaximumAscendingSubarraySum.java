a33b521fe0e826445a2cba65024f81c83b2adfab
fartem <jaman.smlnsk@gmail.com>
2021-04-29 08:13:49 +0300

    2021-04-29 Update: Added "Maximum Ascending Subarray Sum"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/MaximumAscendingSubarraySum.java
@@ -0,0 +1,29 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/maximum-ascending-subarray-sum
+public class MaximumAscendingSubarraySum {
+
+    private final int[] input;
+
+    public MaximumAscendingSubarraySum(int[] input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        int result = 0;
+        int sum = input[0];
+        for (int i = 1; i < input.length; i++) {
+            int num = input[i];
+            int prev = input[i - 1];
+            if (num <= prev) {
+                if (result < sum) {
+                    result = sum;
+                }
+                sum = 0;
+            }
+            sum += num;
+        }
+        return Math.max(result, sum);
+    }
+
+}
