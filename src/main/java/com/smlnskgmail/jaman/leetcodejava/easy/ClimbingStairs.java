7fde9cd17aa56e2006f960c0003c9f6daa946fb1
fartem <jaman.smlnsk@gmail.com>
2021-04-30 08:29:04 +0300

    2021-04-30 Update: Added "Climbing Stairs"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/ClimbingStairs.java
@@ -0,0 +1,26 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/climbing-stairs
+public class ClimbingStairs {
+
+    private final int input;
+
+    public ClimbingStairs(int input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        if (input < 4) {
+            return input;
+        }
+        int prev = 0;
+        int next = 1;
+        for (int i = 0; i < input; ++i) {
+            int sum = prev + next;
+            prev = next;
+            next = sum;
+        }
+        return next;
+    }
+
+}
