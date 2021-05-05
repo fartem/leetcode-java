7810a142f6bf6b4d616d5b09a8ec50ef86649fd0
fartem <jaman.smlnsk@gmail.com>
2021-05-05 22:04:21 +0300

    2021-05-05 Update: Added "Replace All Digits with Characters"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/ReplaceAllDigitsWithCharacters.java
@@ -0,0 +1,22 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/replace-all-digits-with-characters
+public class ReplaceAllDigitsWithCharacters {
+
+    private final String input;
+
+    public ReplaceAllDigitsWithCharacters(String input) {
+        this.input = input;
+    }
+
+    public String solution() {
+        char[] chars = input.toCharArray();
+        for (int i = 1; i < chars.length; i += 2) {
+            int prev = chars[i - 1];
+            int curr = Character.getNumericValue(chars[i]);
+            chars[i] = (char) (prev + curr);
+        }
+        return String.valueOf(chars);
+    }
+
+}
