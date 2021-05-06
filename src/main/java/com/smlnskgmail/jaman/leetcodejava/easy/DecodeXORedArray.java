7d17c40cc472d46e89cc003e6f4a9f2b65ac542d
fartem <jaman.smlnsk@gmail.com>
2021-05-06 09:15:00 +0300

    2021-05-06 Update: Added "Decode XORed Array"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/DecodeXORedArray.java
@@ -0,0 +1,23 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/decode-xored-array
+public class DecodeXORedArray {
+
+    private final int[] encoded;
+    private final int first;
+
+    public DecodeXORedArray(int[] encoded, int first) {
+        this.encoded = encoded;
+        this.first = first;
+    }
+
+    public int[] solution() {
+        int[] result = new int[encoded.length + 1];
+        result[0] = first;
+        for (int i = 1; i < result.length; i++) {
+            result[i] = result[i - 1] ^ encoded[i - 1];
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/DecodeXORedArrayTest.java
@@ -0,0 +1,20 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class DecodeXORedArrayTest {
+
+    @Test
+    public void defaultTest() {
+        assertArrayEquals(
+                new int[]{1, 0, 2, 1},
+                new DecodeXORedArray(
+                        new int[]{1, 2, 3},
+                        1
+                ).solution()
+        );
+    }
+
+}
