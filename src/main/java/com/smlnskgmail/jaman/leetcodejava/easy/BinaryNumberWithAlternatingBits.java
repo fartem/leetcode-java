bee8c5392d048519676d4db735544a7db0cc6659
fartem <jaman.smlnsk@gmail.com>
2021-05-20 07:52:55 +0300

    2021-05-20 Update: Added "Binary Number with Alternating Bits"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,25 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/binary-number-with-alternating-bits
+public class BinaryNumberWithAlternatingBits {
+
+    private int input;
+
+    public BinaryNumberWithAlternatingBits(int input) {
+        this.input = input;
+    }
+
+    public boolean solution() {
+        int curr = input % 2;
+        input /= 2;
+        while (input > 0) {
+            if (curr == input % 2) {
+                return false;
+            }
+            curr = input % 2;
+            input /= 2;
+        }
+        return true;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/BinaryNumberWithAlternatingBitsTest.java
@@ -0,0 +1,16 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BinaryNumberWithAlternatingBitsTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(
+                new BinaryNumberWithAlternatingBits(5).solution()
+        );
+    }
+
+}
