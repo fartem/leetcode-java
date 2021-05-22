2d5f0a7522d0448802aaab26ff90f94494bec8f1
fartem <jaman.smlnsk@gmail.com>
2021-05-22 07:44:53 +0300

    2021-05-22 Update: Added "Sorting the Sentence"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/SortingTheSentence.java
@@ -0,0 +1,32 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/sorting-the-sentence
+public class SortingTheSentence {
+
+    private final String input;
+
+    public SortingTheSentence(String input) {
+        this.input = input;
+    }
+
+    public String solution() {
+        String[] words = input.split(" ");
+        Arrays.sort(
+                words,
+                (s1, s2) -> Character.compare(
+                        s1.charAt(s1.length() - 1),
+                        s2.charAt(s2.length() - 1)
+                )
+        );
+        StringBuilder result = new StringBuilder();
+        for (String word : words) {
+            result
+                    .append(word, 0, word.length() - 1)
+                    .append(" ");
+        }
+        return result.toString().trim();
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/SortingTheSentenceTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SortingTheSentenceTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                "This is a sentence",
+                new SortingTheSentence(
+                        "is2 sentence4 This1 a3"
+                ).solution()
+        );
+    }
+
+}
