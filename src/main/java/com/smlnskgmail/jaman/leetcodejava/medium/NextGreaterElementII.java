54ecba62d124fb70ff1f37298778bf39c8097ce7
fartem <jaman.smlnsk@gmail.com>
2021-05-25 08:11:54 +0300

    2021-05-25 Update: Added "Next Greater Element II"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/NextGreaterElementII.java
@@ -0,0 +1,28 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import java.util.Stack;
+
+// https://leetcode.com/problems/next-greater-element-ii
+public class NextGreaterElementII {
+
+    private final int[] input;
+
+    public NextGreaterElementII(int[] input) {
+        this.input = input;
+    }
+
+    public int[] solution() {
+        int[] result = new int[input.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 2 * input.length - 1; i >= 0; --i) {
+            int index = i % input.length;
+            while (!stack.empty() && input[stack.peek()] <= input[index]) {
+                stack.pop();
+            }
+            result[index] = stack.empty() ? -1 : input[stack.peek()];
+            stack.push(index);
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/NextGreaterElementIITest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class NextGreaterElementIITest {
+
+    @Test
+    public void defaultTest() {
+        assertArrayEquals(
+                new int[]{2, -1, 2},
+                new NextGreaterElementII(
+                        new int[]{1, 2, 1}
+                ).solution()
+        );
+    }
+
+}
