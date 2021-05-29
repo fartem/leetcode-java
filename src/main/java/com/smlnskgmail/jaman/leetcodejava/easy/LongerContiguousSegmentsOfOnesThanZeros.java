7332a58c340d6f6daa17e76460fda05cee2845fb
fartem <jaman.smlnsk@gmail.com>
2021-05-29 07:28:47 +0300

    2021-05-29 update: added "Longer Contiguous Segments of Ones than Zeros"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/LongerContiguousSegmentsOfOnesThanZeros.java
@@ -0,0 +1,40 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/longer-contiguous-segments-of-ones-than-zeros
+public class LongerContiguousSegmentsOfOnesThanZeros {
+
+    private final String input;
+
+    public LongerContiguousSegmentsOfOnesThanZeros(String input) {
+        this.input = input;
+    }
+
+    public boolean solution() {
+        int ones = 0;
+        int zeros = 0;
+        int onesTemp = 0;
+        int zerosTemp = 0;
+        for (int i = 0; i < input.length(); i++) {
+            int digit = input.charAt(i) - '0';
+            if (digit == 1) {
+                if (zerosTemp != 0) {
+                    if (zerosTemp > zeros) {
+                        zeros = zerosTemp;
+                    }
+                    zerosTemp = 0;
+                }
+                onesTemp++;
+            } else {
+                if (onesTemp != 0) {
+                    if (onesTemp > ones) {
+                        ones = onesTemp;
+                    }
+                    onesTemp = 0;
+                }
+                zerosTemp++;
+            }
+        }
+        return Math.max(ones, onesTemp) > Math.max(zeros, zerosTemp);
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/LongerContiguousSegmentsOfOnesThanZerosTest.java
@@ -0,0 +1,18 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class LongerContiguousSegmentsOfOnesThanZerosTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(
+                new LongerContiguousSegmentsOfOnesThanZeros(
+                        "1101"
+                ).solution()
+        );
+    }
+
+}
