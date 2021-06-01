f83f87e73d2229c0bf06a6029599b6261257581e
fartem <jaman.smlnsk@gmail.com>
2021-06-01 07:27:12 +0300

    2021-06-01 update: added "Set Mismatch"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/SetMismatch.java
@@ -0,0 +1,32 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/set-mismatch
+public class SetMismatch {
+
+    private final int[] input;
+
+    public SetMismatch(int[] input) {
+        this.input = input;
+    }
+
+    public int[] solution() {
+        int rep = 0;
+        int[] uniq = new int[input.length + 1];
+        for (int num : input) {
+            if (uniq[num] == 1) {
+                rep = num;
+            } else {
+                uniq[num] = 1;
+            }
+        }
+        int miss = 0;
+        for (int i = 1; i < input.length + 1; i++) {
+            if (uniq[i] == 0) {
+                miss = i;
+                break;
+            }
+        }
+        return new int[]{rep, miss};
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/SetMismatchTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class SetMismatchTest {
+
+    @Test
+    public void defaultTest() {
+        assertArrayEquals(
+                new int[]{2, 3},
+                new SetMismatch(
+                        new int[]{1, 2, 2, 4}
+                ).solution()
+        );
+    }
+
+}
