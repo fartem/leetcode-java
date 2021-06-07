6ddf1c00f9ef9c7841ae0724cca66d44c10df357
fartem <jaman.smlnsk@gmail.com>
2021-06-07 08:44:02 +0300

    2021-06-07 update: added "Find Pivot Index"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/FindPivotIndex.java
@@ -0,0 +1,27 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/find-pivot-index
+public class FindPivotIndex {
+
+    private final int[] input;
+
+    public FindPivotIndex(int[] input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        int sum = 0;
+        int atLeft = 0;
+        for (int num : input) {
+            sum += num;
+        }
+        for (int i = 0; i < input.length; i++) {
+            if (atLeft == sum - atLeft - input[i]) {
+                return i;
+            }
+            atLeft += input[i];
+        }
+        return -1;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/FindPivotIndexTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FindPivotIndexTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                3,
+                new FindPivotIndex(
+                        new int[]{1, 7, 3, 6, 5, 6}
+                ).solution()
+        );
+    }
+
+}
