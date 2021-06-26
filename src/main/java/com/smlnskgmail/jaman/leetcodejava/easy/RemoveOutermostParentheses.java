32859fe1db58e3592c8a61700d707f87112539a3
fartem <jaman.smlnsk@gmail.com>
2021-06-26 08:32:48 +0300

    2021-06-26 update: added "Remove Outermost Parentheses"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/RemoveOutermostParentheses.java
@@ -0,0 +1,27 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/remove-outermost-parentheses
+public class RemoveOutermostParentheses {
+
+    private final String input;
+
+    public RemoveOutermostParentheses(String input) {
+        this.input = input;
+    }
+
+    public String solution() {
+        StringBuilder result = new StringBuilder();
+        int pointer = 0;
+        while (pointer < input.length()) {
+            int startAt = ++pointer;
+            int counter = 1;
+            while (counter != 0) {
+                char c = input.charAt(pointer++);
+                counter += c == '(' ? 1 : -1;
+            }
+            result.append(input, startAt, pointer - 1);
+        }
+        return result.toString();
+    }
+
+}
