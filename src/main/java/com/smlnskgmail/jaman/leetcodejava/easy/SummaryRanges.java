d05153d0c2e9d984c561f608d999277fbec086ed
fartem <jaman.smlnsk@gmail.com>
2021-07-11 07:56:21 +0300

    2021-07-11 update: added "Summary Ranges"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/SummaryRanges.java
@@ -0,0 +1,49 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+// https://leetcode.com/problems/summary-ranges
+public class SummaryRanges {
+
+    private final int[] input;
+
+    public SummaryRanges(int[] input) {
+        this.input = input;
+    }
+
+    public List<String> solution() {
+        if (input.length == 0) {
+            return Collections.emptyList();
+        }
+        List<String> result = new ArrayList<>();
+        int start = input[0];
+        int end = start;
+        for (int i = 1; i < input.length; i++) {
+            int num = input[i];
+            if (end + 1 == num) {
+                end = num;
+            } else {
+                if (start == end) {
+                    result.add(String.valueOf(start));
+                } else {
+                    StringBuilder range = new StringBuilder();
+                    range.append(start).append("->").append(end);
+                    result.add(range.toString());
+                }
+                start = num;
+                end = start;
+            }
+        }
+        if (start == end) {
+            result.add(String.valueOf(start));
+        } else {
+            StringBuilder range = new StringBuilder();
+            range.append(start).append("->").append(end);
+            result.add(range.toString());
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/SummaryRangesTest.java
@@ -0,0 +1,25 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class SummaryRangesTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                Arrays.asList(
+                        "0->2",
+                        "4->5",
+                        "7"
+                ),
+                new SummaryRanges(
+                        new int[]{0, 1, 2, 4, 5, 7}
+                ).solution()
+        );
+    }
+
+}
