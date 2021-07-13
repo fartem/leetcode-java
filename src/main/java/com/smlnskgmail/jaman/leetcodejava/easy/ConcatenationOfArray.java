01d82297654c3ec64cc7395a570d181430f6f4b2
fartem <jaman.smlnsk@gmail.com>
2021-07-13 05:44:26 +0300

    2021-07-13 update: added "Concatenation of Array"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/ConcatenationOfArray.java
@@ -0,0 +1,31 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/concatenation-of-array
+public class ConcatenationOfArray {
+
+    private final int[] input;
+
+    public ConcatenationOfArray(int[] input) {
+        this.input = input;
+    }
+
+    public int[] solution() {
+        int[] result = new int[input.length * 2];
+        System.arraycopy(
+                input,
+                0,
+                result,
+                0,
+                input.length
+        );
+        System.arraycopy(
+                input,
+                0,
+                result,
+                input.length,
+                input.length
+        );
+        return result;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/ConcatenationOfArrayTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class ConcatenationOfArrayTest {
+
+    @Test
+    public void defaultTest() {
+        assertArrayEquals(
+                new int[]{1, 2, 1, 1, 2, 1},
+                new ConcatenationOfArray(
+                        new int[]{1, 2, 1}
+                ).solution()
+        );
+    }
+
+}
