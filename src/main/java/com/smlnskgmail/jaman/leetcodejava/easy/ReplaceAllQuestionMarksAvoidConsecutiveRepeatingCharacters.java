f4e5121090104c84a7ea80ebb7e064c03debd061
fartem <jaman.smlnsk@gmail.com>
2021-07-15 07:02:13 +0300

    2021-07-15 update: added "Replace All ?'s to Avoid Consecutive Repeating Characters"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/ReplaceAllQuestionMarksAvoidConsecutiveRepeatingCharacters.java
@@ -0,0 +1,40 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/replace-all-s-to-avoid-consecutive-repeating-characters
+public class ReplaceAllQuestionMarksAvoidConsecutiveRepeatingCharacters {
+
+    private final String input;
+
+    public ReplaceAllQuestionMarksAvoidConsecutiveRepeatingCharacters(String input) {
+        this.input = input;
+    }
+
+    public String solution() {
+        int offset = -1;
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '?') {
+                offset++;
+                if (offset == 26) {
+                    offset = 0;
+                }
+                char candidate = (char) (97 + offset);
+                char prev = i == 0 ? '0' : input.charAt(i - 1);
+                char next = i == input.length() - 1 ? '0' : input.charAt(i + 1);
+                while (candidate == prev || candidate == next) {
+                    offset++;
+                    if (offset == 26) {
+                        offset = 0;
+                    }
+                    candidate = (char) (97 + offset);
+                }
+                result.append(candidate);
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+
+}
