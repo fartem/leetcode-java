72735006295a8108ef5d053c107fafb2dbd151de
fartem <jaman.smlnsk@gmail.com>
2021-07-23 07:34:07 +0300

    2021-07-23 update: added "Ransom Note"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/RansomNote.java
@@ -0,0 +1,34 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/ransom-note
+public class RansomNote {
+
+    private final String ransomNote;
+    private final String magazine;
+
+    public RansomNote(
+            String ransomNote,
+            String magazine
+    ) {
+        this.ransomNote = ransomNote;
+        this.magazine = magazine;
+    }
+
+    public boolean solution() {
+        int[] letters = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            int index = magazine.charAt(i) - 97;
+            letters[index] = letters[index] + 1;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            int index = ransomNote.charAt(i) - 97;
+            if (letters[index] != 0) {
+                letters[index] = letters[index] - 1;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/RansomNoteTest.java
@@ -0,0 +1,16 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class RansomNoteTest {
+
+    @Test
+    public void defaultTest() {
+        assertFalse(
+                new RansomNote("a", "b").solution()
+        );
+    }
+
+}
