bad9f58571fa02e5007bb854a7da589b44cd4206
fartem <jaman.smlnsk@gmail.com>
2021-07-30 06:37:13 +0300

    2021-07-30 update: added "Final Prices With a Special Discount in a Shop"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/FinalPricesWithASpecialDiscountInAShop.java
@@ -0,0 +1,26 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop
+public class FinalPricesWithASpecialDiscountInAShop {
+
+    private final int[] input;
+
+    public FinalPricesWithASpecialDiscountInAShop(int[] input) {
+        this.input = input;
+    }
+
+    public int[] solution() {
+        for (int i = 0; i < input.length; i++) {
+            int price = input[i];
+            for (int j = i + 1; j < input.length; j++) {
+                int discount = input[j];
+                if (price >= discount) {
+                    input[i] = price - discount;
+                    break;
+                }
+            }
+        }
+        return input;
+    }
+
+}
