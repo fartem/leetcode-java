0bf78eb38283f79c2e437c23e0ddeba1d4db03a0
fartem <jaman.smlnsk@gmail.com>
2021-08-05 17:17:52 +0300

    2021-08-05 update: added "Merge In Between Linked Lists"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/MergeInBetweenLinkedLists.java
@@ -0,0 +1,55 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.ListNode;
+
+// https://leetcode.com/problems/merge-in-between-linked-lists
+public class MergeInBetweenLinkedLists {
+
+    private final ListNode list1;
+    private int a;
+    private int b;
+    private final ListNode list2;
+
+    public MergeInBetweenLinkedLists(
+            ListNode list1,
+            int a,
+            int b,
+            ListNode list2
+    ) {
+        this.list1 = list1;
+        this.a = a;
+        this.b = b;
+        this.list2 = list2;
+    }
+
+    public ListNode solution() {
+        ListNode nodeA = null;
+        ListNode nodeB = null;
+        ListNode pointer = list1;
+        int count = 0;
+        a--;
+        b++;
+        while (pointer != null) {
+            if (count == a - 1) {
+                nodeA = pointer;
+            } else if (count == b + 1) {
+                nodeB = pointer;
+            }
+            count++;
+            pointer = pointer.next;
+        }
+        if (nodeA == null) {
+            return list1;
+        }
+        nodeA.next = list2;
+        if (nodeB != null) {
+            pointer = list2;
+            while (pointer.next != null) {
+                pointer = pointer.next;
+            }
+            pointer.next = nodeB;
+        }
+        return list1;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/MergeInBetweenLinkedListsTest.java
@@ -0,0 +1,73 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.ListNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MergeInBetweenLinkedListsTest {
+
+    @Test
+    public void defaultTest() {
+        ListNode list1 = new ListNode(
+                0,
+                new ListNode(
+                        1,
+                        new ListNode(
+                                2,
+                                new ListNode(
+                                        3,
+                                        new ListNode(
+                                                4,
+                                                new ListNode(
+                                                        5,
+                                                        new ListNode(6)
+                                                )
+                                        )
+                                )
+                        )
+                )
+        );
+        ListNode list2 = new ListNode(
+                1000000,
+                new ListNode(
+                        1000001,
+                        new ListNode(1000002)
+                )
+        );
+        ListNode correct = new ListNode(
+                0,
+                new ListNode(
+                        1,
+                        new ListNode(
+                                2,
+                                new ListNode(
+                                        1000000,
+                                        new ListNode(
+                                                1000001,
+                                                new ListNode(
+                                                        1000002,
+                                                        new ListNode(5)
+                                                )
+                                        )
+                                )
+                        )
+                )
+        );
+        ListNode solution = new MergeInBetweenLinkedLists(
+                list1,
+                3,
+                4,
+                list2
+        ).solution();
+        while (correct != null) {
+            assertEquals(
+                    correct.val,
+                    solution.val
+            );
+            correct = correct.next;
+            solution = solution.next;
+        }
+    }
+
+}
