bc294bad50d6a15374f48f403985e7a5db6ab89b
fartem <jaman.smlnsk@gmail.com>
2021-08-05 16:42:01 +0300

    2021-08-05 update: added "Swap Nodes in Pairs"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/SwapNodesInPairs.java
@@ -0,0 +1,34 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.ListNode;
+
+// https://leetcode.com/problems/swap-nodes-in-pairs
+public class SwapNodesInPairs {
+
+    private ListNode input;
+
+    public SwapNodesInPairs(ListNode input) {
+        this.input = input;
+    }
+
+    public ListNode solution() {
+        if (input == null || input.next == null) {
+            return input;
+        }
+        ListNode start = new ListNode(
+                0,
+                input
+        );
+        ListNode pointer = start;
+        while (pointer.next != null && pointer.next.next != null) {
+            ListNode post = pointer.next.next.next;
+            ListNode last = pointer.next;
+            pointer.next.next.next = pointer.next;
+            pointer.next = pointer.next.next;
+            last.next = post;
+            pointer = last;
+        }
+        return start.next;
+    }
+
+}
