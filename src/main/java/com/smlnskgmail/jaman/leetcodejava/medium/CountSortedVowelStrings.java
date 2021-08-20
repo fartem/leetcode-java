e6dfd27e4967e2fc3dbf13c0b6c8f58d61ae83d0
fartem <jaman.smlnsk@gmail.com>
2021-08-20 19:27:11 +0300

    2021-08-20 update: added "Count Sorted Vowel Strings"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/CountSortedVowelStrings.java
@@ -0,0 +1,16 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+// https://leetcode.com/problems/count-sorted-vowel-strings
+public class CountSortedVowelStrings {
+
+    private final int input;
+
+    public CountSortedVowelStrings(int input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        return (input + 4) * (input + 3) * (input + 2) * (input + 1) / 24;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/CountSortedVowelStringsTest.java
@@ -0,0 +1,17 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CountSortedVowelStringsTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                5,
+                new CountSortedVowelStrings(1).solution()
+        );
+    }
+
+}
