b18bee745a1ed5df64dfb99e824ff0d9e715a661
fartem <jaman.smlnsk@gmail.com>
2021-08-27 20:31:37 +0300

    2021-08-27 update: added "Sum of Left Leaves"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/SumOfLeftLeaves.java
@@ -0,0 +1,34 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+// https://leetcode.com/problems/sum-of-left-leaves
+public class SumOfLeftLeaves {
+
+    private final TreeNode input;
+
+    public SumOfLeftLeaves(TreeNode input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        return sum(input);
+    }
+
+    private int sum(TreeNode root) {
+        int sum = 0;
+        if (root != null) {
+            TreeNode left = root.left;
+            if (left != null) {
+                if (left.left == null && left.right == null) {
+                    sum += left.val;
+                } else {
+                    sum += sum(left);
+                }
+            }
+            sum += sum(root.right);
+        }
+        return sum;
+    }
+
+}
