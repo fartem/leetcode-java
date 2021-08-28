52e206c14415c1af3a2c33ea67f93bb48efa7305
fartem <jaman.smlnsk@gmail.com>
2021-08-28 09:09:04 +0300

    2021-08-28 update: added "Increasing Order Search Tree"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/IncreasingOrderSearchTree.java
@@ -0,0 +1,32 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+public class IncreasingOrderSearchTree {
+
+    private final TreeNode root;
+    private TreeNode curr;
+
+    public IncreasingOrderSearchTree(TreeNode root) {
+        this.root = root;
+    }
+
+    public TreeNode solution() {
+        TreeNode result = new TreeNode();
+        curr = result;
+        inorder(root);
+        return result.right;
+    }
+
+    private void inorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left);
+        node.left = null;
+        curr.right = node;
+        curr = node;
+        inorder(node.right);
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/IncreasingOrderSearchTreeTest.java
@@ -0,0 +1,80 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IncreasingOrderSearchTreeTest {
+
+    @Test
+    public void defaultTest() {
+        TreeNode root = new TreeNode(
+                5,
+                new TreeNode(
+                        3,
+                        new TreeNode(
+                                2,
+                                new TreeNode(1),
+                                null
+                        ),
+                        new TreeNode(4)
+                ),
+                new TreeNode(
+                        6,
+                        null,
+                        new TreeNode(
+                                8,
+                                new TreeNode(7),
+                                new TreeNode(9)
+                        )
+                )
+        );
+        TreeNode correct = new TreeNode(
+                1,
+                null,
+                new TreeNode(
+                        2,
+                        null,
+                        new TreeNode(
+                                3,
+                                null,
+                                new TreeNode(
+                                        4,
+                                        null,
+                                        new TreeNode(
+                                                5,
+                                                null,
+                                                new TreeNode(
+                                                        6,
+                                                        null,
+                                                        new TreeNode(
+                                                                7,
+                                                                null,
+                                                                new TreeNode(
+                                                                        8,
+                                                                        null,
+                                                                        new TreeNode(9)
+                                                                )
+                                                        )
+                                                )
+                                        )
+                                )
+                        )
+                )
+        );
+        TreeNode solution = new IncreasingOrderSearchTree(root).solution();
+        assertTrue(isEquals(correct, solution));
+    }
+
+    private boolean isEquals(TreeNode a, TreeNode b) {
+        if (a != null && b != null) {
+            if (a.val != b.val) {
+                return false;
+            }
+            return isEquals(a.right, b.right);
+        }
+        return a == null && b == null;
+    }
+
+}
