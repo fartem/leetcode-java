6188972a433b8f2c743d0d61d033381c0a238617
fartem <jaman.smlnsk@gmail.com>
2021-09-01 08:19:27 +0300

    2021-09-01 update: added "Merge Two Binary Trees"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/MergeTwoBinaryTrees.java
@@ -0,0 +1,45 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+// https://leetcode.com/problems/merge-two-binary-trees
+public class MergeTwoBinaryTrees {
+
+    private final TreeNode root1;
+    private final TreeNode root2;
+
+    public MergeTwoBinaryTrees(
+            TreeNode root1,
+            TreeNode root2
+    ) {
+        this.root1 = root1;
+        this.root2 = root2;
+    }
+
+    public TreeNode solution() {
+        return merge(root1, root2);
+    }
+
+    private TreeNode merge(
+            TreeNode root1,
+            TreeNode root2
+    ) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        root1.val += root2.val;
+        root1.left = merge(
+                root1.left,
+                root2.left
+        );
+        root1.right = merge(
+                root1.right,
+                root2.right
+        );
+        return root1;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/MergeTwoBinaryTreesTest.java
@@ -0,0 +1,63 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MergeTwoBinaryTreesTest {
+
+    @Test
+    public void defaultTest() {
+        TreeNode root1 = new TreeNode(
+                1,
+                new TreeNode(
+                        3,
+                        new TreeNode(5),
+                        null
+                ),
+                new TreeNode(2)
+        );
+        TreeNode root2 = new TreeNode(
+                2,
+                new TreeNode(
+                        1,
+                        null,
+                        new TreeNode(4)
+                ),
+                new TreeNode(
+                        3,
+                        null,
+                        new TreeNode(7)
+                )
+        );
+        TreeNode correct = new TreeNode(
+                3,
+                new TreeNode(
+                        4,
+                        new TreeNode(5),
+                        new TreeNode(4)
+                ),
+                new TreeNode(
+                        5,
+                        null,
+                        new TreeNode(7)
+                )
+        );
+        TreeNode solution = new MergeTwoBinaryTrees(root1, root2).solution();
+        assertTrue(check(correct, solution));
+    }
+
+    private boolean check(TreeNode left, TreeNode right) {
+        if (left != null && right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            boolean lCorrect = check(left.left, right.left);
+            boolean rCorrect = check(left.right, right.right);
+            return lCorrect && rCorrect;
+        }
+        return left == null && right == null;
+    }
+
+}
