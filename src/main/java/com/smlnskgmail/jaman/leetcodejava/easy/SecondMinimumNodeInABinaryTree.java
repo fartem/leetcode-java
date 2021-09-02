d0090f88908deb409b3230686b890c8c2105f104
fartem <jaman.smlnsk@gmail.com>
2021-09-02 18:12:18 +0300

    2021-09-02 update: added "Second Minimum Node In a Binary Tree"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/SecondMinimumNodeInABinaryTree.java
@@ -0,0 +1,35 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+// https://leetcode.com/problems/second-minimum-node-in-a-binary-tree
+public class SecondMinimumNodeInABinaryTree {
+
+    private final TreeNode input;
+
+    private int fMin;
+    private long sMin = Long.MAX_VALUE;
+
+    public SecondMinimumNodeInABinaryTree(TreeNode input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        fMin = input.val;
+        apply(input);
+        return sMin < Long.MAX_VALUE ? (int) sMin : -1;
+    }
+
+    private void apply(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        if (fMin < node.val && node.val < sMin) {
+            sMin = node.val;
+        } else if (fMin == node.val) {
+            apply(node.left);
+            apply(node.right);
+        }
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/SecondMinimumNodeInABinaryTreeTest.java
@@ -0,0 +1,29 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SecondMinimumNodeInABinaryTreeTest {
+
+    @Test
+    public void defaultTest() {
+        TreeNode root = new TreeNode(
+                2,
+                new TreeNode(2),
+                new TreeNode(
+                        5,
+                        new TreeNode(5),
+                        new TreeNode(7)
+                )
+        );
+        assertEquals(
+                5,
+                new SecondMinimumNodeInABinaryTree(
+                        root
+                ).solution()
+        );
+    }
+
+}
