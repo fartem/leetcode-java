379ecf2284e216af0269a272af28d1bcc7cd334f
fartem <jaman.smlnsk@gmail.com>
2021-09-04 07:37:33 +0300

    2021-09-04 update: added "Convert Sorted Array to Binary Search Tree"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,33 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree
+public class ConvertSortedArrayToBinarySearchTree {
+
+    private final int[] input;
+
+    public ConvertSortedArrayToBinarySearchTree(int[] input) {
+        this.input = input;
+    }
+
+    public TreeNode solution() {
+        return toBST(input, 0, input.length - 1);
+    }
+
+    private TreeNode toBST(
+            int[] nums,
+            int start,
+            int end
+    ) {
+        if (start > end) {
+            return null;
+        }
+        int middle = (start + end) / 2;
+        TreeNode node = new TreeNode(nums[middle]);
+        node.left = toBST(nums, start, middle - 1);
+        node.right = toBST(nums, middle + 1, end);
+        return node;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/ConvertSortedArrayToBinarySearchTreeTest.java
@@ -0,0 +1,43 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ConvertSortedArrayToBinarySearchTreeTest {
+
+    @Test
+    public void defaultTest() {
+        TreeNode correct = new TreeNode(
+                0,
+                new TreeNode(
+                        -10,
+                        null,
+                        new TreeNode(-3)
+                ),
+                new TreeNode(
+                        5,
+                        null,
+                        new TreeNode(9)
+                )
+        );
+        TreeNode solution = new ConvertSortedArrayToBinarySearchTree(
+                new int[]{-10, -3, 0, 5, 9}
+        ).solution();
+        assertTrue(check(correct, solution));
+    }
+
+    private boolean check(TreeNode left, TreeNode right) {
+        if (left != null && right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            boolean lCorrect = check(left.left, right.left);
+            boolean rCorrect = check(left.right, right.right);
+            return lCorrect && rCorrect;
+        }
+        return left == null && right == null;
+    }
+
+}
