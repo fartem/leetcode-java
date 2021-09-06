6183ffe5486d1c65d162d1df3ac144effe86a42f
fartem <jaman.smlnsk@gmail.com>
2021-09-06 07:32:33 +0300

    2021-09-06 update: added "Minimum Depth of Binary Tree"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,35 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+// https://leetcode.com/problems/minimum-depth-of-binary-tree
+public class MinimumDepthOfBinaryTree {
+
+    private final TreeNode input;
+
+    public MinimumDepthOfBinaryTree(TreeNode input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        return minDepth(input);
+    }
+
+    private int minDepth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        if (node.left == null && node.right == null) {
+            return 1;
+        } else if (node.left == null) {
+            return minDepth(node.right) + 1;
+        } else if (node.right == null) {
+            return minDepth(node.left) + 1;
+        }
+        return Math.min(
+                minDepth(node.left) + 1,
+                minDepth(node.right) + 1
+        );
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/MinimumDepthOfBinaryTreeTest.java
@@ -0,0 +1,28 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MinimumDepthOfBinaryTreeTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                2,
+                new MinimumDepthOfBinaryTree(
+                        new TreeNode(
+                                3,
+                                new TreeNode(9),
+                                new TreeNode(
+                                        20,
+                                        new TreeNode(15),
+                                        new TreeNode(7)
+                                )
+                        )
+                ).solution()
+        );
+    }
+
+}
