1f841a91861ae2d52c6646b19cdfbdd5ca9fe53d
fartem <jaman.smlnsk@gmail.com>
2021-09-07 07:26:50 +0300

    2021-09-07 update: added "Path Sum"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/PathSum.java
@@ -0,0 +1,31 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+// https://leetcode.com/problems/path-sum
+public class PathSum {
+
+    private final TreeNode root;
+    private final int targetSum;
+
+    public PathSum(TreeNode root, int targetSum) {
+        this.root = root;
+        this.targetSum = targetSum;
+    }
+
+    public boolean solution() {
+        return sum(root, targetSum, 0);
+    }
+
+    private boolean sum(TreeNode node, int targetSum, int count) {
+        if (node == null) {
+            return false;
+        }
+        if (node.left == null && node.right == null) {
+            return node.val + count == targetSum;
+        }
+        return sum(node.left, targetSum, node.val + count)
+                || sum(node.right, targetSum, node.val + count);
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/PathSumTest.java
@@ -0,0 +1,40 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PathSumTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(
+                new PathSum(
+                        new TreeNode(
+                                5,
+                                new TreeNode(
+                                        4,
+                                        new TreeNode(
+                                                11,
+                                                new TreeNode(7),
+                                                new TreeNode(2)
+                                        ),
+                                        null
+                                ),
+                                new TreeNode(
+                                        8,
+                                        new TreeNode(13),
+                                        new TreeNode(
+                                                4,
+                                                null,
+                                                new TreeNode(1)
+                                        )
+                                )
+                        ),
+                        22
+                ).solution()
+        );
+    }
+
+}
