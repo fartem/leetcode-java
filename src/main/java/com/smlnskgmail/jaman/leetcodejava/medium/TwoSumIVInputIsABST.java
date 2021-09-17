9481cb0c1c28f2d794d2e7e0a0e859dcddcca5a4
fartem <jaman.smlnsk@gmail.com>
2021-09-17 08:07:57 +0300

    2021-09-17 update: added "Two Sum IV - Input is a BST"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/TwoSumIVInputIsABST.java
@@ -0,0 +1,35 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// https://leetcode.com/problems/two-sum-iv-input-is-a-bst
+public class TwoSumIVInputIsABST {
+
+    private final TreeNode root;
+    private final int k;
+
+    public TwoSumIVInputIsABST(TreeNode root, int k) {
+        this.root = root;
+        this.k = k;
+    }
+
+    public boolean solution() {
+        return contains(root, new HashSet<>(), k);
+    }
+
+    private boolean contains(TreeNode node, Set<Integer> nums, int k) {
+        if (node == null) {
+            return false;
+        }
+        int diff = k - node.val;
+        if (nums.contains(diff)) {
+            return true;
+        }
+        nums.add(node.val);
+        return contains(node.left, nums, k) || contains(node.right, nums, k);
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/TwoSumIVInputIsABSTTest.java
@@ -0,0 +1,32 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TwoSumIVInputIsABSTTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(
+                new TwoSumIVInputIsABST(
+                        new TreeNode(
+                                5,
+                                new TreeNode(
+                                        3,
+                                        new TreeNode(2),
+                                        new TreeNode(4)
+                                ),
+                                new TreeNode(
+                                        6,
+                                        null,
+                                        new TreeNode(7)
+                                )
+                        ),
+                        9
+                ).solution()
+        );
+    }
+
+}
