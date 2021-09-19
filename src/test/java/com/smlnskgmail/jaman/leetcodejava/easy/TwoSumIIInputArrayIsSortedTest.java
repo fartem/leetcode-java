a9a65622b4d827c0e85f0ffcaf32dddecacb808a
fartem <jaman.smlnsk@gmail.com>
2021-09-19 08:29:29 +0300

    2021-09-19 update: added "Two Sum II - Input array is sorted"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/TwoSumIIInputArrayIsSortedTest.java
@@ -0,0 +1,20 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class TwoSumIIInputArrayIsSortedTest {
+
+    @Test
+    public void defaultTest() {
+        assertArrayEquals(
+                new int[]{1, 2},
+                new TwoSumIIInputArrayIsSorted(
+                        new int[]{2, 7, 11, 15},
+                        9
+                ).solution()
+        );
+    }
+
+}
