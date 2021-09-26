09cc0c31b683586b3cbba89f0f77a1ce80206956
fartem <jaman.smlnsk@gmail.com>
2021-09-26 07:19:05 +0300

    2021-09-26 update: added "Reverse Prefix of Word"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/ReversePrefixOfWord.java
@@ -0,0 +1,32 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/reverse-prefix-of-word
+public class ReversePrefixOfWord {
+
+    private final String word;
+    private final char ch;
+
+    public ReversePrefixOfWord(String word, char ch) {
+        this.word = word;
+        this.ch = ch;
+    }
+
+    public String solution() {
+        int target = word.indexOf(ch);
+        if (target == -1) {
+            return word;
+        }
+        StringBuilder result = new StringBuilder(word);
+        int start = 0;
+        while (start < target) {
+            char f = result.charAt(start);
+            char s = result.charAt(target);
+            result.setCharAt(start, s);
+            result.setCharAt(target, f);
+            start++;
+            target--;
+        }
+        return result.toString();
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/ReversePrefixOfWordTest.java
@@ -0,0 +1,20 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReversePrefixOfWordTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                "dcbafd",
+                new ReversePrefixOfWord(
+                        "abcdfd",
+                        'd'
+                ).solution()
+        );
+    }
+
+}
