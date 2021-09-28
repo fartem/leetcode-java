d3eb4683db70c88b0af3bd422afbace7795287e3
fartem <jaman.smlnsk@gmail.com>
2021-09-28 07:21:45 +0300

    2021-09-28 update: added "N-ary Tree Level Order Traversal"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/NAryTreeLevelOrderTraversal.java
@@ -0,0 +1,41 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.Node;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+// https://leetcode.com/problems/n-ary-tree-level-order-traversal
+public class NAryTreeLevelOrderTraversal {
+
+    private final Node input;
+
+    public NAryTreeLevelOrderTraversal(Node input) {
+        this.input = input;
+    }
+
+    public List<List<Integer>> solution() {
+        List<List<Integer>> result = new ArrayList<>();
+        if (input == null) {
+            return result;
+        }
+        Deque<Node> queue = new ArrayDeque<>();
+        queue.addLast(input);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = size; i > 0; i--) {
+                Node node = queue.removeFirst();
+                level.add(node.val);
+                for (Node child : node.children) {
+                    queue.addLast(child);
+                }
+            }
+            result.add(level);
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/NAryTreeLevelOrderTraversalTest.java
@@ -0,0 +1,53 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.Node;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class NAryTreeLevelOrderTraversalTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                Arrays.asList(
+                        List.of(1),
+                        Arrays.asList(3, 2, 4),
+                        Arrays.asList(5, 6)
+                ),
+                new NAryTreeLevelOrderTraversal(
+                        new Node(
+                                1,
+                                Arrays.asList(
+                                        new Node(
+                                                3,
+                                                Arrays.asList(
+                                                        new Node(
+                                                                5,
+                                                                Collections.emptyList()
+                                                        ),
+                                                        new Node(
+                                                                6,
+                                                                Collections.emptyList()
+                                                        )
+                                                )
+                                        ),
+                                        new Node(
+                                                2,
+                                                Collections.emptyList()
+                                        ),
+                                        new Node(
+                                                4,
+                                                Collections.emptyList()
+                                        )
+                                )
+                        )
+                ).solution()
+        );
+    }
+
+}
