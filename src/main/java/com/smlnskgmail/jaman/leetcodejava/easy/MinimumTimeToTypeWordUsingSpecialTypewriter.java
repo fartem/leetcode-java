4be00f76bcd00b6047660c8c354f0af13d1a0a1d
fartem <jaman.smlnsk@gmail.com>
2021-10-04 08:32:29 +0300

    2021-10-04 update: added "Minimum Time to Type Word Using Special Typewriter"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/MinimumTimeToTypeWordUsingSpecialTypewriter.java
@@ -0,0 +1,33 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/minimum-time-to-type-word-using-special-typewriter
+public class MinimumTimeToTypeWordUsingSpecialTypewriter {
+
+    private final String input;
+
+    public MinimumTimeToTypeWordUsingSpecialTypewriter(String input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        int result = 0;
+        int prev = 1;
+        for (int i = 0; i < input.length(); i++) {
+            int c = input.charAt(i) - 'a' + 1;
+            if (Math.abs(prev - c) > 13) {
+                if (prev > c) {
+                    int temp = 26 - prev;
+                    result += temp + c + 1;
+                } else {
+                    int temp = 26 - c;
+                    result += temp + prev + 1;
+                }
+            } else {
+                result += Math.abs(prev - c) + 1;
+            }
+            prev = c;
+        }
+        return result;
+    }
+
+}
