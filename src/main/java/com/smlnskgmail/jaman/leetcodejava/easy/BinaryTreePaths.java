d86962c90fa373b3ec54d421f13686adbc4d865f
fartem <jaman.smlnsk@gmail.com>
2021-10-10 11:12:44 +0300

    2021-10-10 update: added "Binary Tree Paths"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/BinaryTreePaths.java
@@ -0,0 +1,42 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/binary-tree-paths
+public class BinaryTreePaths {
+
+    private final TreeNode input;
+
+    public BinaryTreePaths(TreeNode input) {
+        this.input = input;
+    }
+
+    public List<String> solution() {
+        List<String> result = new ArrayList<>();
+        fillPaths(input, "", result);
+        return result;
+    }
+
+    private void fillPaths(
+            TreeNode node,
+            String path,
+            List<String> paths
+    ) {
+        if (node == null) {
+            return;
+        }
+        String curr = path.length() > 0
+                ? path + "->" + node.val
+                : String.valueOf(node.val);
+        if (node.left == null && node.right == null) {
+            paths.add(curr);
+        } else {
+            fillPaths(node.left, curr, paths);
+            fillPaths(node.right, curr, paths);
+        }
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/BinaryTreePathsTest.java
@@ -0,0 +1,33 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class BinaryTreePathsTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                Arrays.asList(
+                        "1->2->5",
+                        "1->3"
+                ),
+                new BinaryTreePaths(
+                        new TreeNode(
+                                1,
+                                new TreeNode(
+                                        2,
+                                        null,
+                                        new TreeNode(5)
+                                ),
+                                new TreeNode(3)
+                        )
+                ).solution()
+        );
+    }
+
+}
