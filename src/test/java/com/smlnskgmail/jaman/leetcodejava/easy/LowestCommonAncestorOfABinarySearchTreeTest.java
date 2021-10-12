c3203155fef043c1d065e675cfd7fadf15a2abc3
fartem <jaman.smlnsk@gmail.com>
2021-10-12 21:11:25 +0300

    2021-10-12 update: added "Lowest Common Ancestor of a Binary Search Tree"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/LowestCommonAncestorOfABinarySearchTreeTest.java
@@ -0,0 +1,38 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LowestCommonAncestorOfABinarySearchTreeTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                6,
+                new LowestCommonAncestorOfABinarySearchTree(
+                        new TreeNode(
+                                6,
+                                new TreeNode(
+                                        2,
+                                        new TreeNode(0),
+                                        new TreeNode(
+                                                4,
+                                                new TreeNode(3),
+                                                new TreeNode(5)
+                                        )
+                                ),
+                                new TreeNode(
+                                        8,
+                                        new TreeNode(7),
+                                        new TreeNode(9)
+                                )
+                        ),
+                        new TreeNode(2),
+                        new TreeNode(8)
+                ).solution().val
+        );
+    }
+
+}
