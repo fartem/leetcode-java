205ceb41357d83eb87f58b36e7a6064d769eff81
fartem <jaman.smlnsk@gmail.com>
2021-10-15 07:52:56 +0300

    2021-10-15 update: added "Construct Binary Search Tree from Preorder Traversal"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/ConstructBinarySearchTreeFromPreorderTraversal.java
@@ -0,0 +1,38 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+// https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal
+public class ConstructBinarySearchTreeFromPreorderTraversal {
+
+    private final int[] input;
+
+    public ConstructBinarySearchTreeFromPreorderTraversal(int[] input) {
+        this.input = input;
+    }
+
+    public TreeNode solution() {
+        return constructBST(input, 0, input.length - 1);
+    }
+
+    private TreeNode constructBST(
+            int[] preorder,
+            int start,
+            int end
+    ) {
+        if (start > end) {
+            return null;
+        }
+        TreeNode node = new TreeNode(preorder[start]);
+        int index;
+        for (index = start; index <= end; index++) {
+            if (preorder[index] > node.val) {
+                break;
+            }
+        }
+        node.left = constructBST(preorder, start + 1, index - 1);
+        node.right = constructBST(preorder, index, end);
+        return node;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/ConstructBinarySearchTreeFromPreorderTraversalTest.java
@@ -0,0 +1,40 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ConstructBinarySearchTreeFromPreorderTraversalTest {
+
+    @Test
+    public void defaultTest() {
+        TreeNode correct = new TreeNode(
+                8,
+                new TreeNode(
+                        5,
+                        new TreeNode(1),
+                        new TreeNode(7)
+                ),
+                new TreeNode(
+                        10,
+                        null,
+                        new TreeNode(12)
+                )
+        );
+        TreeNode solution = new ConstructBinarySearchTreeFromPreorderTraversal(
+                new int[]{8, 5, 1, 7, 10, 12}
+        ).solution();
+        assertTrue(areEquals(correct, solution));
+    }
+
+    private boolean areEquals(TreeNode f, TreeNode s) {
+        if (f != null && s != null) {
+            return f.val == s.val
+                    && areEquals(f.left, s.left)
+                    && areEquals(f.right, s.right);
+        }
+        return f == null && s == null;
+    }
+
+}
