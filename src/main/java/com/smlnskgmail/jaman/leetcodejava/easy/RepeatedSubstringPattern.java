b248e97df20f2f3bc29b43933c21445ce81aae0f
fartem <jaman.smlnsk@gmail.com>
2021-10-27 06:05:16 +0300

    2021-10-27 update: added "Repeated Substring Pattern"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/RepeatedSubstringPattern.java
@@ -0,0 +1,25 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/repeated-substring-pattern
+public class RepeatedSubstringPattern {
+
+    private final String input;
+
+    public RepeatedSubstringPattern(String input) {
+        this.input = input;
+    }
+
+    public boolean solution() {
+        int length = input.length();
+        for (int i = 1; i <= length / 2; i++) {
+            if (length % i == 0) {
+                String sub = input.substring(0, i);
+                if (sub.repeat(length / sub.length()).equals(input)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
