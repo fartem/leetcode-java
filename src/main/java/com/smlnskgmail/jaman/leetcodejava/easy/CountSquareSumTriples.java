7d3fedd7d285bf8a2bb0068135f19b0b4917a9be
fartem <jaman.smlnsk@gmail.com>
2021-11-04 08:58:13 +0300

    2021-11-04 update: added "Count Square Sum Triples"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/CountSquareSumTriples.java
@@ -0,0 +1,27 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/count-square-sum-triples
+public class CountSquareSumTriples {
+
+    private final int input;
+
+    public CountSquareSumTriples(int input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        int result = 0;
+        int limit = input + 1;
+        for (int i = 1; i < limit; i++) {
+            for (int j = 1; j < limit; j++) {
+                int k = i * i + j * j;
+                int sqrt = (int) Math.sqrt(k);
+                if (sqrt * sqrt == k && sqrt < limit) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+}
