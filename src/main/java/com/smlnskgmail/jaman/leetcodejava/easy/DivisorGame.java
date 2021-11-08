0db142277084c57cf1417ee8433b24c685c86240
fartem <jaman.smlnsk@gmail.com>
2021-11-08 07:00:21 +0300

    2021-11-08 update: added "Divisor Game"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/DivisorGame.java
@@ -0,0 +1,16 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/divisor-game
+public class DivisorGame {
+
+    private final int input;
+
+    public DivisorGame(int input) {
+        this.input = input;
+    }
+
+    public boolean solution() {
+        return input % 2 == 0;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/DivisorGameTest.java
@@ -0,0 +1,16 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DivisorGameTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(
+                new DivisorGame(2).solution()
+        );
+    }
+
+}
