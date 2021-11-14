41b7335100802e9cea5747f33ca46cc7d4b94832
fartem <jaman.smlnsk@gmail.com>
2021-11-14 07:26:32 +0300

    2021-11-14 update: added "Backspace String Compare"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/BackspaceStringCompare.java
@@ -0,0 +1,50 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.Stack;
+
+// https://leetcode.com/problems/backspace-string-compare
+public class BackspaceStringCompare {
+
+    private final String s;
+    private final String t;
+
+    public BackspaceStringCompare(
+            String s,
+            String t
+    ) {
+        this.s = s;
+        this.t = t;
+    }
+
+    public boolean solution() {
+        Stack<Character> f = printChars(s);
+        Stack<Character> n = printChars(t);
+        if (f.size() != n.size()) {
+            return false;
+        }
+        for (int i = 0; i < f.size(); i++) {
+            char fC = f.get(i);
+            char nC = n.get(i);
+            if (fC != nC) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Stack<Character> printChars(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '#') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/BackspaceStringCompareTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BackspaceStringCompareTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(
+                new BackspaceStringCompare(
+                        "ab#c",
+                        "ad#c"
+                ).solution()
+        );
+    }
+
+}
