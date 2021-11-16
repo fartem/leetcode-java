6ce1d47706e49504003b822c8c4672b3e47af5dd
fartem <jaman.smlnsk@gmail.com>
2021-11-16 08:29:10 +0300

    2021-11-16 update: added "Make The String Great"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/MakeTheStringGreat.java
@@ -0,0 +1,31 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.Stack;
+
+// https://leetcode.com/problems/make-the-string-great
+public class MakeTheStringGreat {
+
+    private final String input;
+
+    public MakeTheStringGreat(String input) {
+        this.input = input;
+    }
+
+    public String solution() {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (!stack.isEmpty() && Math.abs(stack.peek() - c) == 32) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        char[] result = new char[stack.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = stack.get(i);
+        }
+        return new String(result);
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/MakeTheStringGreatTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MakeTheStringGreatTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                "leetcode",
+                new MakeTheStringGreat(
+                        "leEeetcode"
+                ).solution()
+        );
+    }
+
+}
