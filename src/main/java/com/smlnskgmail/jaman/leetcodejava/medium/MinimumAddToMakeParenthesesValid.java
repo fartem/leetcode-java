f7472e5339b9176339e597fe105abce9267d5d38
fartem <jaman.smlnsk@gmail.com>
2021-11-18 09:20:17 +0300

    2021-11-18 update: added "Minimum Add to Make Parentheses Valid"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,29 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+// https://leetcode.com/problems/minimum-add-to-make-parentheses-valid
+public class MinimumAddToMakeParenthesesValid {
+
+    private final String input;
+
+    public MinimumAddToMakeParenthesesValid(String input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        int o = 0;
+        int c = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == ')') {
+                if (o == 0) {
+                    c++;
+                } else {
+                    o--;
+                }
+            } else {
+                o++;
+            }
+        }
+        return o + c;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/MinimumAddToMakeParenthesesValidTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MinimumAddToMakeParenthesesValidTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                1,
+                new MinimumAddToMakeParenthesesValid(
+                        "())"
+                ).solution()
+        );
+    }
+
+}
