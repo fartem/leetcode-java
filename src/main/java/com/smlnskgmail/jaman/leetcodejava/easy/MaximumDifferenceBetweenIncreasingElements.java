c460d07ebfdaa2eea46177bfe523c632fbdabf3e
fartem <jaman.smlnsk@gmail.com>
2021-11-22 08:32:40 +0300

    2021-11-22 update: added "Maximum Difference Between Increasing Elements"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/MaximumDifferenceBetweenIncreasingElements.java
@@ -0,0 +1,25 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/maximum-difference-between-increasing-elements
+public class MaximumDifferenceBetweenIncreasingElements {
+
+    private final int[] input;
+
+    public MaximumDifferenceBetweenIncreasingElements(int[] input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        int min = input[0];
+        int diff = input[1] - min;
+        for (int num : input) {
+            if (num - min > diff) {
+                diff = num - min;
+            } else if (min > num) {
+                min = num;
+            }
+        }
+        return diff == 0 ? -1 : diff;
+    }
+
+}
