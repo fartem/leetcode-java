83f4669008a41722f067e001f1ace25e0d74d9ce
fartem <jaman.smlnsk@gmail.com>
2021-11-23 07:42:22 +0300

    2021-11-23 update: added "Count Special Quadruplets"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/CountSpecialQuadruplets.java
@@ -0,0 +1,30 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/count-special-quadruplets
+public class CountSpecialQuadruplets {
+
+    private final int[] nums;
+
+    public CountSpecialQuadruplets(int[] input) {
+        this.nums = input;
+    }
+
+    public int solution() {
+        int[] values = new int[101];
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int c = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int index = nums[k] - nums[j] - nums[i];
+                    if (index >= 0) {
+                        result += values[index];
+                    }
+                }
+            }
+            values[c]++;
+        }
+        return result;
+    }
+
+}
