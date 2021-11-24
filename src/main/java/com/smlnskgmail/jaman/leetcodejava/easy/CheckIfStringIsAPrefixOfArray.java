b5ccccbf1992014ba7f6535d4046ed52ae2e7fa5
fartem <jaman.smlnsk@gmail.com>
2021-11-24 08:22:26 +0300

    2021-11-24 update: added "Check If String Is a Prefix of Array"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/CheckIfStringIsAPrefixOfArray.java
@@ -0,0 +1,31 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/check-if-string-is-a-prefix-of-array
+public class CheckIfStringIsAPrefixOfArray {
+
+    private final String s;
+    private final String[] words;
+
+    public CheckIfStringIsAPrefixOfArray(
+            String s,
+            String[] words
+    ) {
+        this.s = s;
+        this.words = words;
+    }
+
+    public boolean solution() {
+        int p = 0;
+        for (int i = 0; i < words.length && p < s.length(); i++) {
+            String word = words[i];
+            for (int j = p, k = 0; j < s.length() && k < word.length(); j++, k++) {
+                if (s.charAt(j) != word.charAt(k)) {
+                    return false;
+                }
+            }
+            p += word.length();
+        }
+        return p == s.length();
+    }
+
+}
