cd938fb4c6056ddfec48fb6fde461883115bc0ec
fartem <jaman.smlnsk@gmail.com>
2021-11-27 13:30:29 +0300

    2021-11-27 update: added "Path Crossing"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/PathCrossing.java
@@ -0,0 +1,44 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.*;
+
+// https://leetcode.com/problems/path-crossing
+public class PathCrossing {
+
+    private final String path;
+
+    public PathCrossing(String input) {
+        this.path = input;
+    }
+
+    public boolean solution() {
+        Map<Integer, Set<Integer>> store = new HashMap<>();
+        store.put(0, new HashSet<>(List.of(0)));
+        for (int i = 0, a = 0, b = 0; i < path.length(); i++) {
+            char moveUnit = path.charAt(i);
+            switch (moveUnit) {
+                case 'N':
+                    a++;
+                    break;
+                case 'S':
+                    a--;
+                    break;
+                case 'E':
+                    b++;
+                    break;
+                case 'W':
+                    b--;
+                    break;
+            }
+            var set = store.getOrDefault(a, new HashSet<>());
+            if (set.contains(b)) {
+                return true;
+            } else {
+                set.add(b);
+                store.put(a, set);
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/PathCrossingTest.java
@@ -0,0 +1,16 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class PathCrossingTest {
+
+    @Test
+    public void defaultTest() {
+        assertFalse(
+                new PathCrossing("NES").solution()
+        );
+    }
+
+}
