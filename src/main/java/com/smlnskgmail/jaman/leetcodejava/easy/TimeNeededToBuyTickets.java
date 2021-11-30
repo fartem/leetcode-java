d6dc3c719a975e8f7fb0864793a1aa34cd88f9c8
fartem <jaman.smlnsk@gmail.com>
2021-11-30 06:39:29 +0300

    2021-11-30 update: added "Time Needed to Buy Tickets"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/TimeNeededToBuyTickets.java
@@ -0,0 +1,28 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/time-needed-to-buy-tickets
+public class TimeNeededToBuyTickets {
+
+    private final int[] tickets;
+    private final int k;
+
+    public TimeNeededToBuyTickets(int[] tickets, int k) {
+        this.tickets = tickets;
+        this.k = k;
+    }
+
+    public int solution() {
+        int result = 0;
+        for (int i = 0; i < tickets.length; i++) {
+            int n = tickets[k];
+            int c = tickets[i];
+            if (i <= k) {
+                result += Math.min(n, c);
+            } else {
+                result += Math.min(n - 1, c);
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/TimeNeededToBuyTicketsTest.java
@@ -0,0 +1,20 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TimeNeededToBuyTicketsTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                6,
+                new TimeNeededToBuyTickets(
+                        new int[]{2, 3, 2},
+                        2
+                ).solution()
+        );
+    }
+
+}
