d97329ae305f8a2dc24abd4a360400d23975e51e
fartem <jaman.smlnsk@gmail.com>
2021-12-11 09:05:24 +0300

    2021-12-11 update: added "Find a Corresponding Node of a Binary Tree in a Clone of That Tree"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree.java
@@ -0,0 +1,43 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+// https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree
+public class FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree {
+
+    private final TreeNode original;
+    private final TreeNode cloned;
+    private final TreeNode target;
+
+    public FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree(
+            TreeNode original,
+            TreeNode cloned,
+            TreeNode target
+    ) {
+        this.original = original;
+        this.cloned = cloned;
+        this.target = target;
+    }
+
+    public TreeNode solution() {
+        return find(cloned, target.val);
+    }
+
+    public TreeNode find(TreeNode node, int value) {
+        if (node != null) {
+            if (node.val == value) {
+                return node;
+            }
+            TreeNode left = find(node.left, value);
+            if (left != null && left.val == value) {
+                return left;
+            }
+            TreeNode right = find(node.right, value);
+            if (right != null && right.val == value) {
+                return right;
+            }
+        }
+        return null;
+    }
+
+}
