ad70eaef9ec5bf30bc4681aba97591e5903c1b08
fartem <jaman.smlnsk@gmail.com>
2021-12-23 08:13:04 +0300

    2021-12-23 update: added "Maximum Difference Between Node and Ancestor"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/MaximumDifferenceBetweenNodeAndAncestor.java
@@ -0,0 +1,29 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+// https://leetcode.com/problems/maximum-difference-between-node-and-ancestor
+public class MaximumDifferenceBetweenNodeAndAncestor {
+
+    private final TreeNode input;
+
+    public MaximumDifferenceBetweenNodeAndAncestor(TreeNode input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        return calculate(input, input.val, input.val);
+    }
+
+    private int calculate(TreeNode node, int min, int max) {
+        if (node != null) {
+            min = Math.min(node.val, min);
+            max = Math.max(node.val, max);
+            int left = calculate(node.left, min, max);
+            int right = calculate(node.right, min, max);
+            return Math.max(left, right);
+        }
+        return max - min;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/MaximumDifferenceBetweenNodeAndAncestorTest.java
@@ -0,0 +1,40 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MaximumDifferenceBetweenNodeAndAncestorTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                7,
+                new MaximumDifferenceBetweenNodeAndAncestor(
+                        new TreeNode(
+                                8,
+                                new TreeNode(
+                                        3,
+                                        new TreeNode(1),
+                                        new TreeNode(
+                                                6,
+                                                new TreeNode(4),
+                                                new TreeNode(7)
+                                        )
+                                ),
+                                new TreeNode(
+                                        10,
+                                        null,
+                                        new TreeNode(
+                                                14,
+                                                new TreeNode(13),
+                                                null
+                                        )
+                                )
+                        )
+                ).solution()
+        );
+    }
+
+}
