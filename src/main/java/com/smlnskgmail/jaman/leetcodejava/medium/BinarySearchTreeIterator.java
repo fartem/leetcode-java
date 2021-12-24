b29f6b0351953c59b2cf9423dbf5e04b1b30cef5
fartem <jaman.smlnsk@gmail.com>
2021-12-24 13:14:36 +0300

    2021-12-24 update: added "Binary Search Tree Iterator"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/BinarySearchTreeIterator.java
@@ -0,0 +1,34 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/binary-search-tree-iterator
+public class BinarySearchTreeIterator {
+
+    private final List<Integer> values = new ArrayList<>();
+    private int pointer = 0;
+
+    public BinarySearchTreeIterator(TreeNode root) {
+        traverse(root);
+    }
+
+    private void traverse(TreeNode node) {
+        if (node != null) {
+            traverse(node.left);
+            values.add(node.val);
+            traverse(node.right);
+        }
+    }
+
+    public int next() {
+        return values.get(pointer++);
+    }
+
+    public boolean hasNext() {
+        return pointer < values.size();
+    }
+
+}
