abda0dae71df1937c00752ed8f081ae0bad14199
fartem <jaman.smlnsk@gmail.com>
2022-01-02 11:31:51 +0300

    2022-01-02 update: added "Check if All A's Appears Before All B's"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/CheckIfAllAsAppearsBeforeAllBs.java
@@ -0,0 +1,16 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/check-if-all-as-appears-before-all-bs
+public class CheckIfAllAsAppearsBeforeAllBs {
+
+    private final String input;
+
+    public CheckIfAllAsAppearsBeforeAllBs(String input) {
+        this.input = input;
+    }
+
+    public boolean solution() {
+        return !input.contains("ba");
+    }
+
+}
