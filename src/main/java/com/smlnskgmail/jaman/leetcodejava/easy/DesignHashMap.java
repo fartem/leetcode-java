961361c59383f548215e15a4f388bb8a52a6de0d
fartem <jaman.smlnsk@gmail.com>
2022-01-03 14:15:30 +0300

    2022-01-03 update: added "Design HashMap"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/DesignHashMap.java
@@ -0,0 +1,85 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.Objects;
+
+// https://leetcode.com/problems/design-hashmap
+public class DesignHashMap {
+
+    private final Entry[] entries = new Entry[500];
+
+    public void put(int key, int value) {
+        int i = indexFor(Objects.hashCode(key), entries.length);
+        Entry e = entries[i];
+        if (e != null) {
+            if (e.key == key) {
+                e.value = value;
+            } else {
+                Entry p = e;
+                while (e != null) {
+                    if (e.key == key) {
+                        e.value = value;
+                        break;
+                    }
+                    p = e;
+                    e = e.next;
+                }
+                if (e == null) {
+                    p.next = new Entry(key, value);
+                }
+            }
+        } else {
+            entries[i] = new Entry(key, value);
+        }
+    }
+
+    private int indexFor(int hash, int length) {
+        return hash & (length - 1);
+    }
+
+    public int get(int key) {
+        int i = indexFor(Objects.hashCode(key), entries.length);
+        Entry e = entries[i];
+        if (e != null) {
+            do {
+                if (e.key == key) {
+                    return e.value;
+                }
+                e = e.next;
+            } while (e != null);
+        }
+        return -1;
+    }
+
+    public void remove(int key) {
+        int i = indexFor(Objects.hashCode(key), entries.length);
+        Entry e = entries[i];
+        if (e != null) {
+            if (e.key == key) {
+                entries[i] = e.next;
+            } else {
+                Entry p = e;
+                e = e.next;
+                while (e != null) {
+                    if (e.key == key) {
+                        p.next = e.next;
+                    }
+                    p = e;
+                    e = e.next;
+                }
+            }
+        }
+    }
+
+    private static class Entry {
+        public int key;
+        public int value;
+        public Entry next;
+
+        public Entry(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/DesignHashMapTest.java
@@ -0,0 +1,34 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DesignHashMapTest {
+
+    @Test
+    public void defaultTest() {
+        var designHashMap = new DesignHashMap();
+        designHashMap.put(1, 1);
+        designHashMap.put(2, 2);
+        assertEquals(
+                1,
+                designHashMap.get(1)
+        );
+        assertEquals(
+                -1,
+                designHashMap.get(3)
+        );
+        designHashMap.put(2, 1);
+        assertEquals(
+                1,
+                designHashMap.get(2)
+        );
+        designHashMap.remove(2);
+        assertEquals(
+                -1,
+                designHashMap.get(2)
+        );
+    }
+
+}
