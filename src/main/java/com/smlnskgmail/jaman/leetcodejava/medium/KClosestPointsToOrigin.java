b6ef769902b2ad8696f00d84f83dbf5b4ba96ce4
fartem <jaman.smlnsk@gmail.com>
2022-01-05 11:28:19 +0300

    2022-01-05 update: added "K Closest Points to Origin"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/KClosestPointsToOrigin.java
@@ -0,0 +1,28 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+// https://leetcode.com/problems/k-closest-points-to-origin
+public class KClosestPointsToOrigin {
+
+    private final int[][] points;
+    private final int k;
+
+    public KClosestPointsToOrigin(int[][] points, int k) {
+        this.points = points;
+        this.k = k;
+    }
+
+    public int[][] solution() {
+        Arrays.sort(points, Comparator.comparingInt(this::distance));
+        int[][] result = new int[k][2];
+        System.arraycopy(points, 0, result, 0, k);
+        return result;
+    }
+
+    private int distance(int[] point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/KClosestPointsToOriginTest.java
@@ -0,0 +1,24 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class KClosestPointsToOriginTest {
+
+    @Test
+    public void defaultTest() {
+        int[][] correct = new int[][]{{2, 2}};
+        int[][] solution = new KClosestPointsToOrigin(
+                new int[][]{{1, 3}, {2, 2}},
+                1
+        ).solution();
+        for (int i = 0; i < correct.length; i++) {
+            assertArrayEquals(
+                    correct[i],
+                    solution[i]
+            );
+        }
+    }
+
+}
