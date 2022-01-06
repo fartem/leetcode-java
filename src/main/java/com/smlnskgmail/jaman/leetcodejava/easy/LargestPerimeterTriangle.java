a0a493fefa185de196631ebde2ea5759c59d699c
fartem <jaman.smlnsk@gmail.com>
2022-01-06 09:48:15 +0300

    2022-01-06 update: added "Largest Perimeter Triangle"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/LargestPerimeterTriangle.java
@@ -0,0 +1,27 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/largest-perimeter-triangle
+public class LargestPerimeterTriangle {
+
+    private final int[] input;
+
+    public LargestPerimeterTriangle(int[] input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        Arrays.sort(input);
+        for (int i = input.length - 1; i >= 2; i--) {
+            int a = input[i - 2];
+            int b = input[i - 1];
+            int c = input[i];
+            if (a + b > c) {
+                return a + b + c;
+            }
+        }
+        return 0;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/LargestPerimeterTriangleTest.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LargestPerimeterTriangleTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                5,
+                new LargestPerimeterTriangle(
+                        new int[]{2, 1, 2}
+                ).solution()
+        );
+    }
+
+}
