b7528ee64e43f053da6541b3b8364e9a0b56560e
fartem <jaman.smlnsk@gmail.com>
2022-01-07 17:40:49 +0300

    2022-01-07 update: added "Positions of Large Groups"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/PositionsOfLargeGroups.java
@@ -0,0 +1,39 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// https://leetcode.com/problems/positions-of-large-groups
+public class PositionsOfLargeGroups {
+
+    private final String input;
+
+    public PositionsOfLargeGroups(String input) {
+        this.input = input;
+    }
+
+    public List<List<Integer>> solution() {
+        List<List<Integer>> result = new ArrayList<>();
+        char prev = input.charAt(0);
+        int count = 1;
+        int length = input.length();
+        for (int i = 1; i < length; i++) {
+            char c = input.charAt(i);
+            if (c == prev) {
+                count++;
+            } else {
+                if (count >= 3) {
+                    result.add(Arrays.asList(i - count, i - 1));
+                }
+                count = 1;
+                prev = c;
+            }
+        }
+        if (count >= 3) {
+            result.add(Arrays.asList(length - count, length - 1));
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/PositionsOfLargeGroupsTest.java
@@ -0,0 +1,22 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class PositionsOfLargeGroupsTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                List.of(Arrays.asList(3, 6)),
+                new PositionsOfLargeGroups(
+                        "abbxxxxzyy"
+                ).solution()
+        );
+    }
+
+}
