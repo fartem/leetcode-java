82dd34a5432d50e66dd06b0cad79292b77314df9
fartem <jaman.smlnsk@gmail.com>
2022-01-11 22:08:36 +0300

    2022-01-11 update: added "Linked List in Binary Tree"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/LinkedListInBinaryTree.java
@@ -0,0 +1,53 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.ListNode;
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+// https://leetcode.com/problems/linked-list-in-binary-tree
+public class LinkedListInBinaryTree {
+
+    private final ListNode head;
+    private final TreeNode root;
+
+    public LinkedListInBinaryTree(ListNode head, TreeNode root) {
+        this.head = head;
+        this.root = root;
+    }
+
+    public boolean solution() {
+        Queue<TreeNode> nodes = new LinkedList<>();
+        nodes.add(root);
+        while (!nodes.isEmpty()) {
+            TreeNode node = nodes.remove();
+            if (node.val == head.val) {
+                boolean isSubPath = dfs(head, node);
+                if (isSubPath) {
+                    return true;
+                }
+            }
+            if (node.left != null) {
+                nodes.add(node.left);
+            }
+            if (node.right != null) {
+                nodes.add(node.right);
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(ListNode head, TreeNode root) {
+        if (head == null) {
+            return true;
+        }
+        if (root == null || root.val != head.val) {
+            return false;
+        }
+        boolean left = dfs(head.next, root.left);
+        boolean right = dfs(head.next, root.right);
+        return left || right;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/LinkedListInBinaryTreeTest.java
@@ -0,0 +1,51 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.ListNode;
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class LinkedListInBinaryTreeTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(
+                new LinkedListInBinaryTree(
+                        new ListNode(
+                                4,
+                                new ListNode(
+                                        2,
+                                        new ListNode(8)
+                                )
+                        ),
+                        new TreeNode(
+                                1,
+                                new TreeNode(
+                                        4,
+                                        null,
+                                        new TreeNode(
+                                                2,
+                                                new TreeNode(1),
+                                                null
+                                        )
+                                ),
+                                new TreeNode(
+                                        4,
+                                        new TreeNode(
+                                                2,
+                                                new TreeNode(6),
+                                                new TreeNode(
+                                                        8,
+                                                        new TreeNode(1),
+                                                        new TreeNode(3)
+                                                )
+                                        ),
+                                        null
+                                )
+                        )
+                ).solution()
+        );
+    }
+
+}
