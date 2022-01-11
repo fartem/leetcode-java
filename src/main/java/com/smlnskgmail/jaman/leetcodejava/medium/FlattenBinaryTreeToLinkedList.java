fdb94cf9dd6e5b2479053fe0690757d24d577cba
fartem <jaman.smlnsk@gmail.com>
2022-01-11 08:01:13 +0300

    2022-01-11 update: added "Flatten Binary Tree to Linked List"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,40 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+// https://leetcode.com/problems/flatten-binary-tree-to-linked-list
+public class FlattenBinaryTreeToLinkedList {
+
+    private final TreeNode input;
+
+    public FlattenBinaryTreeToLinkedList(TreeNode input) {
+        this.input = input;
+    }
+
+    public void solution() {
+        fillValues(input);
+    }
+
+    private void fillValues(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            return;
+        }
+        if (root.left != null) {
+            fillValues(root.left);
+            TreeNode temp = root.right;
+            root.right = root.left;
+            root.left = null;
+
+            TreeNode tail = root.right;
+            while (tail.right != null) {
+                tail = tail.right;
+            }
+            tail.right = temp;
+        }
+        fillValues(root.right);
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/FlattenBinaryTreeToLinkedListTest.java
@@ -0,0 +1,54 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FlattenBinaryTreeToLinkedListTest {
+
+    @Test
+    public void defaultTest() {
+        TreeNode solution = new TreeNode(
+                1,
+                new TreeNode(
+                        2,
+                        new TreeNode(3),
+                        new TreeNode(4)
+                ),
+                new TreeNode(
+                        5,
+                        null,
+                        new TreeNode(6)
+                )
+        );
+        new FlattenBinaryTreeToLinkedList(solution).solution();
+        assertTrue(
+                TreeNode.areEqual(
+                        new TreeNode(
+                                1,
+                                null,
+                                new TreeNode(
+                                        2,
+                                        null,
+                                        new TreeNode(
+                                                3,
+                                                null,
+                                                new TreeNode(
+                                                        4,
+                                                        null,
+                                                        new TreeNode(
+                                                                5,
+                                                                null,
+                                                                new TreeNode(6)
+                                                        )
+                                                )
+                                        )
+                                )
+                        ),
+                        solution
+                )
+        );
+    }
+
+}
