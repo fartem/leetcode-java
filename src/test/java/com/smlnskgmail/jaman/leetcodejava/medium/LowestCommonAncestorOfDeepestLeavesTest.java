17f6a844989f1bcb21f7cabd7bb5590454ee998c
fartem <jaman.smlnsk@gmail.com>
2022-01-14 07:37:18 +0300

    2022-01-14 update: added "Lowest Common Ancestor of Deepest Leaves"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/LowestCommonAncestorOfDeepestLeavesTest.java
@@ -0,0 +1,42 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class LowestCommonAncestorOfDeepestLeavesTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(
+                TreeNode.areEqual(
+                        new TreeNode(
+                                2,
+                                new TreeNode(7),
+                                new TreeNode(4)
+                        ),
+                        new LowestCommonAncestorOfDeepestLeaves(
+                                new TreeNode(
+                                        3,
+                                        new TreeNode(
+                                                5,
+                                                new TreeNode(6),
+                                                new TreeNode(
+                                                        2,
+                                                        new TreeNode(7),
+                                                        new TreeNode(4)
+                                                )
+                                        ),
+                                        new TreeNode(
+                                                1,
+                                                new TreeNode(0),
+                                                new TreeNode(8)
+                                        )
+                                )
+                        ).solution()
+                )
+        );
+    }
+
+}
