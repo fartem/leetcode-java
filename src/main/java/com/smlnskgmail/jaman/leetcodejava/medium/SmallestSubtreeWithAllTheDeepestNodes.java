4de8e27565cef151ad83dab2827f116f36f43e48
fartem <jaman.smlnsk@gmail.com>
2022-01-15 09:08:41 +0300

    2022-01-15 update: added "Smallest Subtree with all the Deepest Nodes"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/SmallestSubtreeWithAllTheDeepestNodes.java
@@ -0,0 +1,43 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+
+// https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes
+public class SmallestSubtreeWithAllTheDeepestNodes {
+
+    private final TreeNode input;
+
+    public SmallestSubtreeWithAllTheDeepestNodes(TreeNode input) {
+        this.input = input;
+    }
+
+    public TreeNode solution() {
+        int depth = depth(input);
+        return dfs(input, depth);
+    }
+
+    private int depth(TreeNode node) {
+        if (node != null) {
+            int left = depth(node.left);
+            int right = depth(node.right);
+            return 1 + Math.max(left, right);
+        }
+        return 0;
+    }
+
+    private TreeNode dfs(TreeNode node, int depth) {
+        if (node != null) {
+            if (depth == 1) {
+                return node;
+            }
+            TreeNode left = dfs(node.left, depth - 1);
+            TreeNode right = dfs(node.right, depth - 1);
+            if (left != null && right != null) {
+                return node;
+            }
+            return left != null ? left : right;
+        }
+        return null;
+    }
+
+}
