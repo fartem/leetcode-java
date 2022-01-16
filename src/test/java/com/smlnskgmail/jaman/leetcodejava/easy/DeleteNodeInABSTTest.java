1f5bdb7ab8fd2d1c3f9ee907fb75c5b55111ff3e
fartem <jaman.smlnsk@gmail.com>
2022-01-16 14:58:37 +0300

    2022-01-16 update: added "Delete Node in a BST"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/DeleteNodeInABSTTest.java
@@ -0,0 +1,47 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DeleteNodeInABSTTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(
+                TreeNode.areEqual(
+                        new TreeNode(
+                                5,
+                                new TreeNode(
+                                        4,
+                                        new TreeNode(2),
+                                        null
+                                ),
+                                new TreeNode(
+                                        6,
+                                        null,
+                                        new TreeNode(7)
+                                )
+                        ),
+                        new DeleteNodeInABST(
+                                new TreeNode(
+                                        5,
+                                        new TreeNode(
+                                                4,
+                                                new TreeNode(2),
+                                                null
+                                        ),
+                                        new TreeNode(
+                                                6,
+                                                null,
+                                                new TreeNode(7)
+                                        )
+                                ),
+                                3
+                        ).solution()
+                )
+        );
+    }
+
+}
