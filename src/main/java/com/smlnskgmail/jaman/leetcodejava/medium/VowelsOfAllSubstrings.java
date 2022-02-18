08d6888ee4ace18ba69cbaf6b272fefe455adb77
fartem <jaman.smlnsk@gmail.com>
2022-02-18 21:44:46 +0300

    2022-02-18 update: added "Vowels of All Substrings"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/VowelsOfAllSubstrings.java
@@ -0,0 +1,27 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+// https://leetcode.com/problems/vowels-of-all-substrings
+public class VowelsOfAllSubstrings {
+
+    private final String input;
+
+    public VowelsOfAllSubstrings(String input) {
+        this.input = input;
+    }
+
+    public long solution() {
+        long result = 0;
+        int length = input.length();
+        for (int i = 0; i < length; i++) {
+            if (isVowel(input.charAt(i))) {
+                result += (long) (length - i) * (long) (i + 1);
+            }
+        }
+        return result;
+    }
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/VowelsOfAllSubstringsTest.java
@@ -0,0 +1,17 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VowelsOfAllSubstringsTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                6,
+                new VowelsOfAllSubstrings("aba").solution()
+        );
+    }
+
+}
