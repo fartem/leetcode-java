65fe09e7ee2b4b9fb0cb029c95b81f80ec5cda44
fartem <jaman.smlnsk@gmail.com>
2022-02-20 11:18:03 +0300

    2022-02-20 update: updated "Remove Covered Intervals"

--- src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/RemoveCoveredIntervals.java
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/RemoveCoveredIntervals.java
@@ -5,17 +5,17 @@ import java.util.Arrays;
 // https://leetcode.com/problems/remove-covered-intervals
 public class RemoveCoveredIntervals {
 
-    private final int[][] intervals;
+    private final int[][] input;
 
     public RemoveCoveredIntervals(int[][] input) {
-        this.intervals = input;
+        this.input = input;
     }
 
     public int solution() {
-        Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        Arrays.sort(input, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
         int count = 0;
         int curr = 0;
-        for (int[] interval : intervals) {
+        for (int[] interval : input) {
             if (curr < interval[1]) {
                 curr = interval[1];
                 count++;
