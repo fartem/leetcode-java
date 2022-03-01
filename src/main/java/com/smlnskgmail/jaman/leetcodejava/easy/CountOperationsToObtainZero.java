bc0491d1e7e37adc3ff81681069ebdfe4643212f
fartem <jaman.smlnsk@gmail.com>
2022-03-01 10:48:15 +0300

    2022-03-01 update: added "Count Operations to Obtain Zero"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/CountOperationsToObtainZero.java
@@ -0,0 +1,30 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/count-operations-to-obtain-zero
+public class CountOperationsToObtainZero {
+
+    private int num1;
+    private int num2;
+
+    public CountOperationsToObtainZero(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int solution() {
+        int num = Math.min(num1, num2);
+        int result = 0;
+        while (num != 0) {
+            if (num1 >= num2) {
+                num = num1 - num2;
+                num1 = num;
+            } else {
+                num = num2 - num1;
+                num2 = num;
+            }
+            result++;
+        }
+        return result;
+    }
+
+}
