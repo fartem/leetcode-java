ec1ae57be6ffdb537650604683b960d7b25fddeb
fartem <jaman.smlnsk@gmail.com>
2022-03-06 10:32:34 +0300

    2022-03-06 update: added "Count Integers With Even Digit Sum"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/CountIntegersWithEvenDigitSum.java
@@ -0,0 +1,29 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/count-integers-with-even-digit-sum
+public class CountIntegersWithEvenDigitSum {
+
+    private final int input;
+
+    public CountIntegersWithEvenDigitSum(int input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        int result = 0;
+        for (int i = 1; i <= input; i++) {
+            int curr = i;
+            int sum = 0;
+            while (curr != 0) {
+                int part = curr % 10;
+                sum += part;
+                curr /= 10;
+            }
+            if (sum % 2 == 0) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+}
