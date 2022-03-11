f512fcb2c50ae8b0b7bbc539df0273cd351a90bd
fartem <jaman.smlnsk@gmail.com>
2022-03-11 20:37:06 +0300

    2022-03-11 update: updated "Perfect Number"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/PerfectNumberTest.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PerfectNumberTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(new PerfectNumber(28).solution());
+    }
+
+}
