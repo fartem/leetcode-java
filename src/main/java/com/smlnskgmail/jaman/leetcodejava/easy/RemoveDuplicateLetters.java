ddf83ad41015a976231f21f79fd63ea3312e73fe
fartem <jaman.smlnsk@gmail.com>
2022-03-20 08:23:38 +0300

    2022-03-20 update: added "Remove Duplicate Letters"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/RemoveDuplicateLetters.java
@@ -0,0 +1,38 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import java.util.Stack;
+
+// https://leetcode.com/problems/remove-duplicate-letters
+public class RemoveDuplicateLetters {
+
+    private final String s;
+
+    public RemoveDuplicateLetters(String input) {
+        this.s = input;
+    }
+
+    public String solution() {
+        int[] lastIndices = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            lastIndices[s.charAt(i) - 'a'] = i;
+        }
+        boolean[] seen = new boolean[26];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i) - 'a';
+            if (!seen[c]) {
+                while (!stack.isEmpty() && stack.peek() > c && i < lastIndices[stack.peek()]) {
+                    seen[stack.pop()] = false;
+                }
+                stack.push(c);
+                seen[c] = true;
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            result.append((char) (stack.pop() + 'a'));
+        }
+        return result.reverse().toString();
+    }
+
+}
