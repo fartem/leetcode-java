8aec6c9cfbf5b541742a0bcc562f833580c55861
fartem <jaman.smlnsk@gmail.com>
2022-04-01 21:34:51 +0300

    2022-04-01 update: updated "Maximum Product of Word Lengths"

--- src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/MaximumProductOfWordLengths.java
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/MaximumProductOfWordLengths.java
@@ -6,28 +6,28 @@ import java.util.Map;
 // https://leetcode.com/problems/maximum-product-of-word-lengths
 public class MaximumProductOfWordLengths {
 
-    private final String[] words;
+    private final String[] input;
 
     public MaximumProductOfWordLengths(String[] input) {
-        this.words = input;
+        this.input = input;
     }
 
     public int solution() {
         int result = 0;
         Map<String, int[]> letters = new HashMap<>();
-        for (String word : words) {
+        for (String word : input) {
             int[] l = new int[26];
             for (int j = 0; j < word.length(); j++) {
                 l[word.charAt(j) - 97]++;
             }
             letters.put(word, l);
         }
-        for (int i = 0; i < words.length; i++) {
-            String word = words[i];
+        for (int i = 0; i < input.length; i++) {
+            String word = input[i];
             int[] wLetters = letters.get(word);
-            for (int k = 0; k < words.length; k++) {
+            for (int k = 0; k < input.length; k++) {
                 if (k != i) {
-                    String candidate = words[k];
+                    String candidate = input[k];
                     int[] cLetters = letters.get(candidate);
                     boolean correct = true;
                     for (int m = 0; m < wLetters.length; m++) {
