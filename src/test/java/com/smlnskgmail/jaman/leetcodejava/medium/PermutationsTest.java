e1912203a25c7ac3bedb22ac1ac432da90ff08f0
fartem <jaman.smlnsk@gmail.com>
2022-04-02 08:57:35 +0300

    2022-04-02 update: addded "Permutations"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/PermutationsTest.java
@@ -0,0 +1,26 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class PermutationsTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                List.of(
+                        List.of(1, 2, 3),
+                        List.of(1, 3, 2),
+                        List.of(2, 1, 3),
+                        List.of(2, 3, 1),
+                        List.of(3, 1, 2),
+                        List.of(3, 2, 1)
+                ),
+                new Permutations(new int[]{1, 2, 3}).solution()
+        );
+    }
+
+}
