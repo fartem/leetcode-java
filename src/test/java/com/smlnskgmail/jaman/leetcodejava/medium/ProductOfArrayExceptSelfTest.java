b28b292c146d72978aad189926361751424c3404
fartem <jaman.smlnsk@gmail.com>
2022-04-03 10:31:53 +0300

    2022-04-03 update: added "Product of Array Except Self"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/ProductOfArrayExceptSelfTest.java
@@ -0,0 +1,21 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductOfArrayExceptSelfTest {
+
+    @Test
+    public void defaultTest() {
+        assertArrayEquals(
+                new int[]{24, 12, 8, 6},
+                new ProductOfArrayExceptSelf(new int[]{1, 2, 3, 4}).solution()
+        );
+        assertArrayEquals(
+                new int[]{0, 0, 9, 0, 0},
+                new ProductOfArrayExceptSelf(new int[]{-1, 1, 0, -3, 3}).solution()
+        );
+    }
+
+}
