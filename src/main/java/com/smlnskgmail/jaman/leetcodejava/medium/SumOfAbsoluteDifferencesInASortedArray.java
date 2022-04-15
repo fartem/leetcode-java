5df4fe6da3e1f9b18a72c90bc350ce442e540343
fartem <jaman.smlnsk@gmail.com>
2022-04-15 06:46:35 +0300

    2022-04-15 update: updated "Sum of Absolute Differences in a Sorted Array"

--- src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/SumOfAbsoluteDifferencesInASortedArray.java
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/SumOfAbsoluteDifferencesInASortedArray.java
@@ -3,23 +3,23 @@ package com.smlnskgmail.jaman.leetcodejava.medium;
 // https://leetcode.com/problems/sum-of-absolute-differences-in-a-sorted-array
 public class SumOfAbsoluteDifferencesInASortedArray {
 
-    private final int[] nums;
+    private final int[] input;
 
     public SumOfAbsoluteDifferencesInASortedArray(int[] input) {
-        this.nums = input;
+        this.input = input;
     }
 
     public int[] solution() {
-        int n = nums.length;
+        int n = input.length;
         for (int i = 1; i < n; i++) {
-            nums[i] += nums[i - 1];
+            input[i] += input[i - 1];
         }
         int[] result = new int[n];
-        result[0] = nums[n - 1] - n * nums[0];
+        result[0] = input[n - 1] - n * input[0];
         for (int i = 1; i < n; i++) {
-            int left = nums[i - 1];
-            int right = nums[n - 1] - nums[i];
-            int current = nums[i] - left;
+            int left = input[i - 1];
+            int right = input[n - 1] - input[i];
+            int current = input[i] - left;
             result[i] = i * current - left + right - (n - i - 1) * current;
         }
         return result;
