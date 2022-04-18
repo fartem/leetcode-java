162cdf2f60a34ab8910cb263f5108f761d687e53
fartem <jaman.smlnsk@gmail.com>
2022-04-18 06:52:07 +0300

    2022-04-18 update: added "Minimum Moves to Equal Array Elements II"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/MinimumMovesToEqualArrayElementsII.java
@@ -0,0 +1,25 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii
+public class MinimumMovesToEqualArrayElementsII {
+
+    private final int[] input;
+
+    public MinimumMovesToEqualArrayElementsII(int[] input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        Arrays.sort(input);
+        int n = input.length;
+        int m = n % 2 == 0 ? (input[n / 2] + input[n / 2 - 1]) / 2 : input[n / 2];
+        int result = 0;
+        for (int num : input) {
+            result += Math.abs(m - num);
+        }
+        return result;
+    }
+
+}
