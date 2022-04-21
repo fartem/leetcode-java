0771b552828d5cccefa5c82ff722d1f71ca0a4e9
fartem <jaman.smlnsk@gmail.com>
2022-04-21 06:35:06 +0300

    2022-04-21 update: added "Maximum XOR for Each Query"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/MaximumXORForEachQueryTest.java
@@ -0,0 +1,20 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class MaximumXORForEachQueryTest {
+
+    @Test
+    public void defaultTest() {
+        assertArrayEquals(
+                new int[]{0, 3, 2, 3},
+                new MaximumXORForEachQuery(
+                        new int[]{0, 3, 2, 3},
+                        2
+                ).solution()
+        );
+    }
+
+}
