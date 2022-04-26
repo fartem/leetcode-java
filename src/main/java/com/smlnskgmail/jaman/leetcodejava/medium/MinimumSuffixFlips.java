0d9773029fbc1fa36c2fe151129860577bf1f44f
fartem <jaman.smlnsk@gmail.com>
2022-04-26 08:23:51 +0300

    2022-04-26 update: added "Minimum Suffix Flips"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/MinimumSuffixFlips.java
@@ -0,0 +1,24 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+// https://leetcode.com/problems/minimum-suffix-flips
+public class MinimumSuffixFlips {
+
+    private final String target;
+
+    public MinimumSuffixFlips(String input) {
+        this.target = input;
+    }
+
+    public int solution() {
+        char flip = '0';
+        int result = 0;
+        for (int i = 0; i < target.length(); i++) {
+            if (target.charAt(i) != flip) {
+                flip = target.charAt(i);
+                result++;
+            }
+        }
+        return result;
+    }
+
+}
