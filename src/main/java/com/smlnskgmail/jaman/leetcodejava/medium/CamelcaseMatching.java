fadab33604abb77b8d79391669198f4ccd3d7ab6
fartem <jaman.smlnsk@gmail.com>
2022-05-01 07:26:01 +0300

    2022-05-01 update: added "Camelcase Matching"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/CamelcaseMatching.java
@@ -0,0 +1,39 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/camelcase-matching
+public class CamelcaseMatching {
+
+    private final String[] queries;
+    private final String pattern;
+
+    public CamelcaseMatching(String[] queries, String pattern) {
+        this.queries = queries;
+        this.pattern = pattern;
+    }
+
+    public List<Boolean> solution() {
+        List<Boolean> result = new ArrayList<>(queries.length);
+        int pLength = pattern.length();
+        for (String query : queries) {
+            int p = 0;
+            int i;
+            for (i = 0; i < query.length(); i++) {
+                char c = query.charAt(i);
+                if (p >= pLength && c >= 'A' && c <= 'Z') {
+                    break;
+                }
+                if (p < pLength && c == pattern.charAt(p)) {
+                    p++;
+                } else if (c >= 'A' && c <= 'Z' && c != pattern.charAt(p)) {
+                    break;
+                }
+            }
+            result.add(i == query.length() && p == pLength);
+        }
+        return result;
+    }
+
+}
