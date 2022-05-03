27fc4cb0076760f25f4119170ea015d6a4cda941
fartem <jaman.smlnsk@gmail.com>
2022-05-03 07:38:07 +0300

    2022-05-03 update: added "Shortest Unsorted Continuous Subarray"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,28 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/shortest-unsorted-continuous-subarray
+public class ShortestUnsortedContinuousSubarray {
+
+    private final int[] input;
+
+    public ShortestUnsortedContinuousSubarray(int[] input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        int[] sorted = input.clone();
+        Arrays.sort(sorted);
+        int start = input.length;
+        int end = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] != sorted[i]) {
+                start = Math.min(start, i);
+                end = Math.max(end, i);
+            }
+        }
+        return end - start >= 0 ? end - start + 1 : 0;
+    }
+
+}
