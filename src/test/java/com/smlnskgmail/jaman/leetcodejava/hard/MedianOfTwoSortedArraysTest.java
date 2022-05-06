59c11fc253df0dd99b588cbcba68bd5a9a4546c3
fartem <jaman.smlnsk@gmail.com>
2022-05-06 07:44:11 +0300

    2022-05-06 update: updated "Median of Two Sorted Arrays"

--- src/test/java/com/smlnskgmail/jaman/leetcodejava/hard/MedianOfTwoSortedArraysTest.java
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/hard/MedianOfTwoSortedArraysTest.java
@@ -8,19 +8,11 @@ public class MedianOfTwoSortedArraysTest {
 
     @Test
     public void defaultTest() {
-//        assertEquals(
-//                2.0000,
-//                new MedianOfTwoSortedArrays(
-//                        new int[]{1, 3},
-//                        new int[]{2}
-//                ).solution(),
-//                0
-//        );
         assertEquals(
-                2.5000,
+                2.0000,
                 new MedianOfTwoSortedArrays(
-                        new int[]{1, 2},
-                        new int[]{3, 4}
+                        new int[]{1, 3},
+                        new int[]{2}
                 ).solution(),
                 0
         );
