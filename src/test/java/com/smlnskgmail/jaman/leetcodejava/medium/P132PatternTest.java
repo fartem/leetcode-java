5df3556a2cdaa76139782a84c73c80b200440ee4
fartem <jaman.smlnsk@gmail.com>
2022-05-07 19:46:54 +0300

    2022-05-07 update: added "132 Pattern"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/P132PatternTest.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class P132PatternTest {
+
+    @Test
+    public void defaultTest() {
+        assertFalse(new P132Pattern(new int[]{1, 2, 3, 4}).solution());
+    }
+
+}
