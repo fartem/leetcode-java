cf620df451cbf067e2cc8f54ea62f0a0f3d7ee38
fartem <jaman.smlnsk@gmail.com>
2022-05-19 18:54:38 +0300

    2022-05-19 update: added "Unique Binary Search Trees"

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/medium/UniqueBinarySearchTrees.java
@@ -0,0 +1,24 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+// https://leetcode.com/problems/unique-binary-search-trees
+public class UniqueBinarySearchTrees {
+
+    private final int input;
+
+    public UniqueBinarySearchTrees(int input) {
+        this.input = input;
+    }
+
+    public int solution() {
+        int[] catalan = new int[input + 2];
+        catalan[0] = 1;
+        catalan[1] = 1;
+        for (int i = 2; i <= input; i++) {
+            for (int j = 0; j < i; j++) {
+                catalan[i] += catalan[j] * catalan[i - j - 1];
+            }
+        }
+        return catalan[input];
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/UniqueBinarySearchTreesTest.java
@@ -0,0 +1,17 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UniqueBinarySearchTreesTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(
+                5,
+                new UniqueBinarySearchTrees(3).solution()
+        );
+    }
+
+}
