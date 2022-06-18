69622d6ce9b7c3ec12143181e977ab90b9972432
fartem <jaman.smlnsk@gmail.com>
2022-06-18 10:53:03 +0300

    2022-06-18 update: formatted code

--- src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/CheckCompletenessOfABinaryTreeTest.java
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/CheckCompletenessOfABinaryTreeTest.java
@@ -3,7 +3,7 @@ package com.smlnskgmail.jaman.leetcodejava.medium;
 import com.smlnskgmail.jaman.leetcodejava.support.TreeNode;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class CheckCompletenessOfABinaryTreeTest {
 
