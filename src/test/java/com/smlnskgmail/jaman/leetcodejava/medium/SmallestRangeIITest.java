27202b727f508d958accea291eeaa25de9422366
fartem <jaman.smlnsk@gmail.com>
2022-07-05 06:44:44 +0300

    2022-07-05 update: added "Smallest Range II"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/SmallestRangeIITest.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SmallestRangeIITest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(0, new SmallestRangeII(new int[]{1}, 0).solution());
+    }
+
+}
