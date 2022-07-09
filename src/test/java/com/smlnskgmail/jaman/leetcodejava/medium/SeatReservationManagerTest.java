d6cdc56d33f5779913d50efeded32696a5da919d
fartem <jaman.smlnsk@gmail.com>
2022-07-09 18:27:03 +0300

    2022-07-09 update: added "Seat Reservation Manager"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/SeatReservationManagerTest.java
@@ -0,0 +1,21 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SeatReservationManagerTest {
+
+    @Test
+    public void defaultTest() {
+        var seatReservationManager = new SeatReservationManager(5);
+        assertEquals(1, seatReservationManager.reserve());
+        assertEquals(2, seatReservationManager.reserve());
+        seatReservationManager.unreserve(2);
+        assertEquals(2, seatReservationManager.reserve());
+        assertEquals(3, seatReservationManager.reserve());
+        assertEquals(4, seatReservationManager.reserve());
+        assertEquals(5, seatReservationManager.reserve());
+    }
+
+}
