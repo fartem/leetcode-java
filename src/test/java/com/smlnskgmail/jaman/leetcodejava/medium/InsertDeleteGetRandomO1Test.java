590297e0a40c8b5250da825d2b419bf7bb4519d8
Artem Fomchenkov <45129887+fartem@users.noreply.github.com>
2022-07-12 10:44:59 +0300

    2022-07-12 update: added "Insert Delete GetRandom O(1)" (#33)

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/InsertDeleteGetRandomO1Test.java
@@ -0,0 +1,25 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InsertDeleteGetRandomO1Test {
+
+    @Test
+    public void defaultTest() {
+        var insertDeleteGetRandomO1 = new InsertDeleteGetRandomO1();
+        assertTrue(insertDeleteGetRandomO1.insert(1));
+        assertFalse(insertDeleteGetRandomO1.remove(2));
+        assertTrue(insertDeleteGetRandomO1.insert(2));
+
+        int random = insertDeleteGetRandomO1.getRandom();
+        assertTrue(random == 1 || random == 2);
+
+        assertTrue(insertDeleteGetRandomO1.remove(1));
+        assertFalse(insertDeleteGetRandomO1.insert(2));
+
+        assertEquals(2, insertDeleteGetRandomO1.getRandom());
+    }
+
+}
