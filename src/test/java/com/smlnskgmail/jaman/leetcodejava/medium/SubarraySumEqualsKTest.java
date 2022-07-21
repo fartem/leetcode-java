8c423162b1c207480e5041eb9e69eaebd493531b
Artem Fomchenkov <45129887+fartem@users.noreply.github.com>
2022-07-21 07:53:43 +0300

    2022-07-21 update: added "Subarray Sum Equals K" (#44)

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/SubarraySumEqualsKTest.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SubarraySumEqualsKTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(2, new SubarraySumEqualsK(new int[]{1, 1, 1}, 2).solution());
+    }
+
+}
