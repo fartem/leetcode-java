f01e2b34da7b07be86fc0c0c481d45d839665216
fartem <jaman.smlnsk@gmail.com>
2022-07-30 07:46:41 +0300

    2022-07-30 update: added "Find the Winner of the Circular Game"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/FindTheWinnerOfTheCircularGameTest.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FindTheWinnerOfTheCircularGameTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(3, new FindTheWinnerOfTheCircularGame(5, 2).solution());
+    }
+
+}
