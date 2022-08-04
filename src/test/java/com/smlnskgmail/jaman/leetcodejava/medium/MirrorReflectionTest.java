6a0b598a64f76fb3a70e5a7efa5b796dfc8fbc24
Artem Fomchenkov <45129887+fartem@users.noreply.github.com>
2022-08-04 07:59:13 +0300

    2022-08-04 update: added "Mirror Reflection" (#56)

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/MirrorReflectionTest.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MirrorReflectionTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(2, new MirrorReflection(2, 1).solution());
+    }
+
+}
