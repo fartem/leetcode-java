9d93d701458d1c32488658b060364fd28c877e23
Artem Fomchenkov <45129887+fartem@users.noreply.github.com>
2022-08-05 06:41:29 +0300

    2022-08-05 update: added "Combination Sum IV" (#59)

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/CombinationSumIVTest.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CombinationSumIVTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(7, new CombinationSumIV(new int[]{1, 2, 3}, 4).solution());
+    }
+
+}
