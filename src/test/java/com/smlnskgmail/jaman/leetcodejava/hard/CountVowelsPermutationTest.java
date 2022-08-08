627f6af89a4de0834aecabe750d29dcd81192495
fartem <jaman.smlnsk@gmail.com>
2022-08-08 06:42:40 +0300

    2022-08-08 update: added "Count Vowels Permutation"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/hard/CountVowelsPermutationTest.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.hard;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CountVowelsPermutationTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(5, new CountVowelsPermutation(1).solution());
+    }
+
+}
