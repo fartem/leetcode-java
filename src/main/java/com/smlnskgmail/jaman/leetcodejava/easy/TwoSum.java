6e3807c159fe7e8bc182e856f01eab4f8cc435f9
fartem <jaman.smlnsk@gmail.com>
2022-08-16 07:11:00 +0300

    2022-08-15 update: updated "Two Sum"

--- src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/TwoSum.java
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/TwoSum.java
@@ -1,21 +1,42 @@
 package com.smlnskgmail.jaman.leetcodejava.easy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 // https://leetcode.com/problems/two-sum/
 public class TwoSum {
 
-    private final int[] array;
+    private final int[] nums;
     private final int target;
 
     public TwoSum(int[] array, int target) {
-        this.array = array;
+        this.nums = array;
         this.target = target;
     }
 
     public int[] solution() {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if (array[i] + array[j] == target) {
-                    return new int[]{i, j};
+        Map<Integer, List<Integer>> numbers = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (numbers.containsKey(num)) {
+                numbers.get(num).add(i);
+            } else {
+                List<Integer> indices = new ArrayList<>();
+                indices.add(i);
+                numbers.put(num, indices);
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+            if (numbers.containsKey(diff)) {
+                var indices = numbers.get(diff);
+                for (int index : indices) {
+                    if (index != i) {
+                        return new int[]{i, index};
+                    }
                 }
             }
         }
