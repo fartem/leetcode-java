6e1bde10ffabb27b8a378309a4a2c916121a06ff
fartem <jaman.smlnsk@gmail.com>
2022-08-19 06:53:11 +0300

    2022-08-19 update: added "Split Array into Consecutive Subsequences"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/SplitArrayIntoConsecutiveSubsequencesTest.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SplitArrayIntoConsecutiveSubsequencesTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(new SplitArrayIntoConsecutiveSubsequences(new int[]{1, 2, 3, 3, 4, 5}).solution());
+    }
+
+}
