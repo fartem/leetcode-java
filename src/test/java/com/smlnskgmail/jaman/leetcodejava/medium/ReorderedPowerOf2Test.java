cf7eb54f95f0009ee2fad4f962c3343c6a131036
fartem <jaman.smlnsk@gmail.com>
2022-08-26 07:51:39 +0300

    2022-08-26 update: added "Reordered Power of 2"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/ReorderedPowerOf2Test.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ReorderedPowerOf2Test {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(new ReorderedPowerOf2(1).solution());
+    }
+
+}
