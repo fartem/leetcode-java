75a1e5ec3b82e5d1fa28cf4c52975706925e49ad
Artem Fomchenkov <45129887+fartem@users.noreply.github.com>
2022-09-08 13:22:10 +0300

    2022-09-08 update: added "Valid Parenthesis String" (#87)

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/ValidParenthesisStringTest.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ValidParenthesisStringTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(new ValidParenthesisString("()").solution());
+    }
+
+}
