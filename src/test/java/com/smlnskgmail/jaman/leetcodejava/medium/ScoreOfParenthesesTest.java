9a6c0dd1f2d121ced4946c768d485ad54f69c25b
Artem Fomchenkov <45129887+fartem@users.noreply.github.com>
2022-09-11 08:00:32 +0300

    2022-09-11 update: added "Score of Parentheses" (#90)

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/medium/ScoreOfParenthesesTest.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ScoreOfParenthesesTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(1, new ScoreOfParentheses("()").solution());
+    }
+
+}
