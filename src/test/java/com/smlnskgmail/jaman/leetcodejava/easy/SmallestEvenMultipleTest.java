fc71d7ed2535ac0ebbf5850028840d9ad5f42235
Artem Fomchenkov <45129887+fartem@users.noreply.github.com>
2022-09-20 08:03:19 +0300

    2022-09-20 update: added "Smallest Even Multiple" (#112)

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/SmallestEvenMultipleTest.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SmallestEvenMultipleTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(10, new SmallestEvenMultiple(5).solution());
+    }
+
+}
