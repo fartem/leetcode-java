fb8dfc97bce19285257456e6628730afee05a7ce
Artem Fomchenkov <45129887+fartem@users.noreply.github.com>
2022-10-04 08:04:43 +0300

    2022-10-04 update: added "Find the Middle Index in Array" (#122)

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/MaximumScoreAfterSplittingAStringTest.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MaximumScoreAfterSplittingAStringTest {
+
+    @Test
+    public void defaultTest() {
+        assertEquals(5, new MaximumScoreAfterSplittingAString("011101").solution());
+    }
+
+}
