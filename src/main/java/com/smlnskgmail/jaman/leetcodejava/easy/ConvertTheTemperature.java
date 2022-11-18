7b8b8c5a2fadbb1e84ffe37fec042ef8c0155713
Artem Fomchenkov <45129887+fartem@users.noreply.github.com>
2022-11-18 19:19:06 +0300

    2022-11-18 update: added "2469. Convert the Temperature" (#137)

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/ConvertTheTemperature.java
@@ -0,0 +1,19 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/convert-the-temperature/description/
+public class ConvertTheTemperature {
+
+    private final double input;
+
+    public ConvertTheTemperature(double celsius) {
+        this.input = celsius;
+    }
+
+    public double[] solution() {
+        return new double[]{
+                input + 273.15,
+                input * 1.80 + 32.00
+        };
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/ConvertTheTemperatureTest.java
@@ -0,0 +1,18 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConvertTheTemperatureTest {
+
+    @Test
+    public void defaultTest() {
+        assertArrayEquals(
+                new double[]{309.65000, 97.70000},
+                new ConvertTheTemperature(36.50).solution(),
+                0
+        );
+    }
+
+}
