f56ebe05168e69c80aded86a5ee970f15e2e407c
fartem <jaman.smlnsk@gmail.com>
2022-11-18 19:08:33 +0300

    2022-11-18 update: added "263. Ugly Number"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/UglyNumberTest.java
@@ -0,0 +1,14 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class UglyNumberTest {
+
+    @Test
+    public void defaultTest() {
+        assertTrue(new UglyNumber(6).solution());
+    }
+
+}
