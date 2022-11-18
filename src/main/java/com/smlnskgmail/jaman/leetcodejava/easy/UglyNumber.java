f40a0e3516bbb2372c10cc1ac73a8423ea718243
Artem Fomchenkov <45129887+fartem@users.noreply.github.com>
2022-11-18 19:11:23 +0300

    2022-11-18 update: added "263. Ugly Number" (#136)

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/UglyNumber.java
@@ -0,0 +1,24 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/ugly-number/description/
+public class UglyNumber {
+
+    private int input;
+
+    public UglyNumber(int input) {
+        this.input = input;
+    }
+
+    public boolean solution() {
+        if (input <= 0) {
+            return false;
+        }
+        for (int div : new int[]{2, 3, 5}) {
+            while (input % div == 0) {
+                input /= div;
+            }
+        }
+        return input == 1;
+    }
+
+}
