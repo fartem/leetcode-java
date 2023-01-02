49dbbe293dcf7f798192995ab725969a539c1a6e
Artem Fomchenkov <45129887+fartem@users.noreply.github.com>
2023-01-02 08:06:58 +0300

    2023-01-02 update: added "2515. Shortest Distance to Target String in a Circular Array" (#144)

---
+++ src/main/java/com/smlnskgmail/jaman/leetcodejava/easy/ShortestDistanceToTargetStringInACircularArray.java
@@ -0,0 +1,32 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+// https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/description/
+public class ShortestDistanceToTargetStringInACircularArray {
+
+    private final String[] words;
+    private final String target;
+    private final int startIndex;
+
+    public ShortestDistanceToTargetStringInACircularArray(
+            String[] words,
+            String target,
+            int startIndex
+    ) {
+        this.words = words;
+        this.target = target;
+        this.startIndex = startIndex;
+    }
+
+    public int solution() {
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(target)) {
+                int l = Math.abs(i - startIndex);
+                result = Math.min(result, l);
+                result = Math.min(result, words.length - l);
+            }
+        }
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
+
+}
