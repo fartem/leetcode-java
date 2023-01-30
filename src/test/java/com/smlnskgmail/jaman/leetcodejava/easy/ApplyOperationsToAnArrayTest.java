b82128a035a1f50ab5b4e138d1741ce10f661a46
fartem <jaman.smlnsk@gmail.com>
2023-01-30 08:37:53 +0300

    2023-01-30 update: added "2460. Apply Operations to an Array"

---
+++ src/test/java/com/smlnskgmail/jaman/leetcodejava/easy/ApplyOperationsToAnArrayTest.java
@@ -0,0 +1,25 @@
+package com.smlnskgmail.jaman.leetcodejava.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class ApplyOperationsToAnArrayTest {
+
+    @Test
+    public void defaultTests() {
+        assertArrayEquals(
+                new int[]{1, 4, 2, 0, 0, 0},
+                new ApplyOperationsToAnArray(
+                        new int[]{1, 2, 2, 1, 1, 0}
+                ).solution()
+        );
+        assertArrayEquals(
+                new int[]{0, 1},
+                new ApplyOperationsToAnArray(
+                        new int[]{1, 0}
+                ).solution()
+        );
+    }
+
+}
