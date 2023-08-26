7104d9d790cecf54a2db1eea980d47e72c9edbe1
fartem <jaman.smlnsk@gmail.com>
2023-08-26 13:46:25 +0300

    2023-08-26 update: added lcsp support and refactored some files

---
+++ lcsp/finder.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module LCSP
+  # Solutions finder.
+  class LCSPFinder
+    # @param {String} path
+    # @param {String} number
+    def initialize(path, number)
+      @path = path
+      @number = number
+    end
+
+    # @return {String}
+    def solution
+      lines = ::File.readlines("#{@path}/README.md").reject { |line| line.include?(@number) }
+
+      return if lines.empty?
+
+      line = lines.first
+
+      lines[line.rindex('[Link]'), line.rindex(')')]
+    end
+  end
+end
